494c2f641fa8b08741b8bf57e029ba2a99ec6c9c
waqas1981 <w.zeshan@tap.company>
2022-11-04 18:00:10 +0500

    Best seller adde

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-    protected $fillable = ["name","slug", "product_id", "status", "description", "code", "images", "category_id","retail_price", "stockAvailable", "brand"];
+    protected $fillable = ["name","slug", "product_id", "status", "description", "code", "images", "category_id","retail_price", "stockAvailable", "brand", "views"];
     public function categories()
     {
         return $this->belongsTo('App\Models\Category','category_id');
