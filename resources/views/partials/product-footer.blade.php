b17f2fc1d1f36b32112c5a0254feac1fa0f8a134
iqrariqqi741 <iqrariqqi741@gmail.com>
2022-11-14 13:02:19 +0500

    footer layout

--- resources/views/partials/product-footer.blade.php
+++ resources/views/partials/product-footer.blade.php
@@ -64,7 +64,7 @@
 	}
 </style>
 
-<footer class="text-white text-lg-start bg-dark h-50">
+<footer class="text-white text-lg-start bg-dark h-900">
 	<div class="container-fluid">
 		<div class="row ">
 			<div class=" col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 text-dark ">
@@ -73,16 +73,17 @@
 
 			<div class="col-xl-3 col-lg-3 col-md-0 col-sm-0 col-xs-0 main-page-footer-section footer-section-product">
 				<h5 class="mb-4 contact-us-footer">CONTACT US</h5>
-				<span><img src="/theme/img/map-pin.png" class="img-fluid"></span><span class="ms-2 fw-bold">Los Angeles
+				<span><img src="/theme/img/map-pin.png" class="img-fluid"></span><span class="ms-2 pra-footer">Los
+					Angeles
 					Warehouse
 					(HQ)</span>
 				<p class="ms-4 ms-4-footer">&nbsp;
 					13310 S. Figueroa St. Los Angeles, CA 90061
 				</p>
-				<span><img src="/theme/img/phone-call.png" class="img-fluid"></span><span class="ms-2 fw-bold">(916)
+				<span><img src="/theme/img/phone-call.png" class="img-fluid"></span><span class="ms-2 pra-footer">(916)
 					281-3090</span>
 				<p class="ms-4 ms-4-footer">&nbsp;
-					<span class="text-success">
+					<span class="text-success sm-4-footer ">
 						MONDAY-SUNDAY 7 Dayes</span> 9AM-5pm
 				</p>
 
@@ -95,10 +96,12 @@
 				class="col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 mb-4 mb-md-0 main-page-footer-section footer-section-product">
 				<div class="d-flex">
 					<div class="justify-content-end">
-						<h5 class="text-uppercase mb-4">Customer Service</h5>
-						<p class="card-text justify-content-end">Returns</p>
-						<p class="card-text justify-content-end">Customer Support</p>
-						<p class="card-text justify-content-end">Hydro Guide and Tips</p>
+						<h5 class="text-uppercase mb-4 contact-us-footer">Customer Service</h5>
+						<p class="card-text justify-content-end sm-4-footer">Returns</p>
+						<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+						<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+						<h5 class="text-uppercase mb-4 contact-us-footer">MY ACCOUNTS</h5>
+						<p class="card-text justify-content-end sm-4-footer">My orders</p>
 					</div>
 				</div>
 			</div>
@@ -108,11 +111,16 @@
 			</div>
 
 			<div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product">
-				<h5 class=" text-uppercase mb-4"> PAYMENT METHOD</h5>
+				<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
 				<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
 				<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
 				<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
 				<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
+			</div>
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+				<p class="mt-4">
+					copayright @ La Garden Supplay. All right reserved
+				</p>
 			</div>
 
 		</div>
