f89036e3eaabc3b1554bcbca71f2beedffd96ec5
iqrariqqi741 <iqrariqqi741@gmail.com>
2022-11-17 18:08:17 +0500

    responsive

--- resources/views/partials/top-bar.blade.php
+++ resources/views/partials/top-bar.blade.php
@@ -1,7 +1,6 @@
 <header class="p-2 bg-dark text-white top-bar-height w-100">
 
-    <div class="container">
-
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-4"></div>
             <div class="col-md-2">
@@ -9,6 +8,7 @@
 
                 </div>
             </div>
+
             <div class="col-md-4 d-flex align-items-center justify-content-end">
                 <a href="{{'/user/'}}" class="text-white d-flex align-items-end mt-2">
                     <div>
@@ -31,8 +31,9 @@
                     @endif
                 </a>
             </div>
+
             <div class="col-md-2 d-flex justify-content-center align-items-center mt-2">
-                <div>
+                <div style="font-family: 'Poppins';">
                     <img class="basket-icon" src="/theme/img/Bascket.png">
                     <span
                         class="cart-basket d-flex align-items-center justify-content-center float-sm-end cart-counter rounded-circle"
@@ -55,11 +56,9 @@
                         @endif
                         {{$total_quantity}}
 
-
-
-
                     </span>
                 </div>
+
                 <div>
                     <a class="p-0 cart-price btn btn-secondary" data-bs-container="body" data-bs-toggle="popover"
                         data-bs-placement="right"
@@ -70,6 +69,7 @@
                                     class="cart-counter-details">{{$total_quantity}}</span>&nbsp;<span
                                     class="cart-counter-details">items</span> )</a>
                 </div>
+
             </div>
         </div>
     </div>
