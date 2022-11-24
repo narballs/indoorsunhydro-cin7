dfa57f9a6f994577766684d2d825dbaf59bec429
iqrariqqi741 <iqrariqqi741@gmail.com>
2022-11-24 19:16:20 +0500

    thank you page

--- resources/views/checkout/order-received.blade.php
+++ resources/views/checkout/order-received.blade.php
@@ -2,130 +2,115 @@
 @include('partials.top-bar')
 @include('partials.search-bar')
 
-<?php 
-session()->forget('cart');
+{{session()->forget('cart');}}
 
-?>
-<div class="mb-5">
-	<p style="line-height: 95px;" class="fw-bold fs-2 product-btn my-auto border-0 text-white text-center align-middle">
-		Thank You
-	</p>
-</div>
 <div class="container">
 	<div class="row">
-		<table class="mb-5">
-			<tr>
-				<th>Order number</th>
-				<th>Date</th>
-				<th>Email</th>
-				<th>Total</th>
-				<th>Payment Method</th>
-			</tr>
-			<tr>
-				<td>{{$order->id}}</td>
-				<td>{{$formatedDate}}</td>
-				<td>{{$order->user->email}}</td>
-				<td>$ {{$order->total}}</td>
-				<td>{{$order->paymentTerms}}</td>
-			</tr>
-		</table>
-		<table class="mt-5">
-			<tr>
-				<th>PRODUCTS</th>
-			</tr>
+		<div class="col-md-12">
+			<div class="card">
+				<div class="card-header">
+					<h5>card header</h5>
+				</div>
+				<div class="card-body">
+					<div class="row">
+						<div class="col-md-6">
+							<div class="billing-address bg-light p-3 ms-4 mt-5 mb-5">
+								<div class="bg-light">
+									<div style="font-weight: 600;font-size: 20px;">Billing Address</div>
+									<div class="row mt-2">
+										<div class="col-md-6 name">{{$order->user->contact->firstName}}
+											{{$order->user->contact->lastName}}</div>
+										<div class="col-md-6 name">{{$order->user->contact->company}}</div>
+									</div>
+								</div>
+								<div class="address-line bg-light name">
+									{{$order->user->contact->postalAddress1}}
+								</div>
 
-			@foreach($order->apiOrderItem as $item)
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr class="border-bottom mt-5">
-				<td class="mt-3">
-					<a
-						href="{{ url('product-detail/'. $item->product->id) }}">{{$item->product->name}}</a>&nbsp;&nbsp;&nbsp;&nbsp;
-					X {{$item->quantity}}
-				</td>
-			</tr>
-			@endforeach
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr class="border-bottom">
-				<th>Subtotal</th>
-				<td><strong>${{$order->total}}</strong></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr class="border-bottom">
-				<th>Shipping Method</th>
-				<td><strong>$0.00</strong></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr class="border-bottom">
-				<th>Payment Method</th>
-				<td><strong>{{$order->paymentTerms}}</strong></td>
-			</tr>
-		</table>
-		<div class="col-md-5 " id="shipping_address">
-			<div class="billing-address bg-light p-3 ms-4 mt-5 mb-5">
-				<div class="bg-light">
-					<div style="font-weight: 600;font-size: 20px;">Billing Address</div>
-					<div class="row mt-2">
-						<div class="col-md-6 name">{{$order->user->contact->firstName}}</div>
-						<div class="col-md-6 name">{{$order->user->contact->lastName}}</div>
+								<div class="address-line bg-light name">
+									{{$order->user->contact->postalAddress2}}
+								</div>
+
+								<div class="billing-address bg-light">
+									<div class="row m-0">
+										<div class="col p-0 name">
+											{{$order->user->contact->postalCity}}
+										</div>
+										<div class="col p-0 name">
+											{{$order->user->contact->postalState}}
+										</div>
+										<div class="col p-0 name">
+											{{$order->user->contact->postalPostCode}}
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="billing-address bg-light p-3 ms-4 mt-5 mb-5">
+								<div class="bg-light">
+									<div style="font-weight: 600;font-size: 20px;">Shipping Address</div>
+									<div class="row mt-2">
+										<div class="col-md-6 name">{{$order->user->contact->firstName}}
+											{{$order->user->contact->lastName}}</div>
+										<div class="col-md-6 name">{{$order->user->contact->company}}</div>
+									</div>
+								</div>
+								<div class="address-line bg-light name">
+									{{$order->user->contact->postalAddress1}}
+								</div>
+
+
+								<div class="address-line bg-light name">
+									{{$order->user->contact->postalAddress2}}
+								</div>
+								<div class="billing-address bg-light">
+									<div class="row m-0">
+										<div class="col p-0 name">
+											{{$order->user->contact->postalCity}}
+										</div>
+										<div class="col p-0 name">
+											{{$order->user->contact->postalState}}
+										</div>
+										<div class="col p-0 name">
+											{{$order->user->contact->postalPostCode}}
+										</div>
+									</div>
+								</div>
+
+							</div>
+						</div>
+
+						<div class="col-md-12">
+							<p class="fs-1">Items Purchased</p>
+						</div>
+						@foreach($order->apiOrderItem as $item)
+						<div class="row">
+							<div class="col-md-2">
+								1
+								<img src="/theme/img/price.php" alt="">
+							</div>
+							<div class="col-md-6">
+								<a class="thnak-you-page-product-name"
+									href="{{ url('product-detail/'. $item->product->id) }}">{{$item->product->name}}</a>
+							</div>
+							<div class="col-md-2">
+								<p>{{$item->quantity}}</p>
+							</div>
+							<div class="col-md-2">
+								<p class="thank-you-page-product-price">${{$item->product->retail_price}}</p>
+							</div>
+						</div>
+						@endforeach
+
 					</div>
 				</div>
-				<div class="address-line bg-light name">
-					{{$order->user->contact->postalAddress1}}
-				</div>
-
-
-				<div class="address-line bg-light name">
-					{{$order->user->contact->postalAddress2}}
-				</div>
-				<div class="billing-address bg-light">
-					<div class="row m-0">
-						<div class="col p-0 name">
-							{{$order->user->contact->postalCity}}
-						</div>
-						<div class="col p-0 name">
-							{{$order->user->contact->postalState}}
-						</div>
-						<div class="col p-0 name">
-							{{$order->user->contact->postalPostCode}}
-						</div>
-					</div>
-				</div>
-
 			</div>
 		</div>
 	</div>
 </div>
+
+
 @include('partials.product-footer')
 @include('partials.footer')
