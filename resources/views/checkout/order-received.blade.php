65bca0f019fd578e98f3e6a9553dbaa261fb4fc2
iqrariqqi741 <iqrariqqi741@gmail.com>
2022-11-29 15:03:16 +0500

    thankyoupage

--- resources/views/checkout/order-received.blade.php
+++ resources/views/checkout/order-received.blade.php
@@ -1,24 +1,29 @@
 @include('partials.header')
-
+@include('partials.top-bar')
+@include('partials.search-bar')
 {{session()->forget('cart');}}
 
 <div class="container mt-5">
 	<div class="row">
 		<div class="col-md-12">
-			<div class="card">
-				<div class="card-header">
+			<div class="card  border-0">
+				<div class="card-header bg-white border-0 mt-3">
 					<div class="row">
-						<div class="col-md-6">
+						<div class="col-md-5">
 							<h5 class="fw-bold fs-1" style="font-family: 'Poppins'">Order Confirmed</h5>
 						</div>
 						<div class="col-md-6 mt-1">
-							<p style="font-family: 'Poppins'"> Thank You
+							<p style="font-family: 'Poppins'"> <span><img src="/theme/img/star.png" alt=""></span> Thank
+								You
 								<span class="fw-bold">
 									{{$order->user->contact->firstName}}
 									{{$order->user->contact->lastName}}
 								</span>
-								Your Order has been recelved!
+								Your Order has been resolved!
 							</p>
+						</div>
+						<div class="col-md-1 pb-2">
+							<img src="/theme/img/crose.png" alt="">
 						</div>
 					</div>
 				</div>
@@ -29,70 +34,85 @@
 								<div class="bg-light">
 									<div style="font-weight: 600;font-size: 20px;">Billing Address</div>
 									<div class="row mt-2">
-										<div class="col-md-6 name">{{$order->user->contact->firstName}}
-											{{$order->user->contact->lastName}}</div>
-										<div class="col-md-6 name">{{$order->user->contact->company}}</div>
+										<div class="col-md-6 name">
+											{{$order->user->contact->firstName}}
+											{{$order->user->contact->lastName}}<br>
+										</div>
+										<div class="col-md-6 name">
+											{{$order->user->contact->company}}
+										</div>
 									</div>
 								</div>
-								<div class="address-line bg-light name">
-									{{$order->user->contact->postalAddress1}}
-								</div>
-
-								<div class="address-line bg-light name">
+								<div class="address-line bg-light name col-md-6">
+									{{$order->user->contact->postalAddress1}}<br>
 									{{$order->user->contact->postalAddress2}}
 								</div>
-
-								<div class="billing-address bg-light">
-									<div class="row m-0">
-										<div class="col p-0 name">
+								<div class="row">
+									<div class="col-md-4 mb-0">
+										<p class="name mb-0">City</p>
+										<div class="address-line bg-light name">
 											{{$order->user->contact->postalCity}}
 										</div>
-										<div class="col p-0 name">
+
+									</div>
+									<div class="col-md-4 mb-0">
+										<P class="name mb-0">State</P>
+										<div class="address-line bg-light name">
 											{{$order->user->contact->postalState}}
 										</div>
-										<div class="col p-0 name">
-											{{$order->user->contact->postalPostCode}}
-										</div>
+
+									</div>
+									<div class="col-md-4 mb-0">
+										<p class="name mb-0">Zip</p>
+										<div class="address-line bg-light name">
+											{{$order->user->contact->postalPostCode}}</div>
+
 									</div>
 								</div>
-
 							</div>
 						</div>
 						<div class="col-md-6">
 							<div class="billing-address bg-light p-3 ms-4 mt-5 mb-5">
 								<div class="bg-light">
-									<div style="font-weight: 600;font-size: 20px;">Shipping Address</div>
+									<div style="font-weight: 600;font-size: 20px;">Billing Address</div>
 									<div class="row mt-2">
-										<div class="col-md-6 name">{{$order->user->contact->firstName}}
-											{{$order->user->contact->lastName}}</div>
-										<div class="col-md-6 name">{{$order->user->contact->company}}</div>
+										<div class="col-md-6 name">
+											{{$order->user->contact->firstName}}
+											{{$order->user->contact->lastName}}<br>
+										</div>
+										<div class="col-md-6 name">
+											{{$order->user->contact->company}}
+										</div>
 									</div>
 								</div>
-								<div class="address-line bg-light name">
-									{{$order->user->contact->postalAddress1}}
-								</div>
-
-
-								<div class="address-line bg-light name">
+								<div class="address-line bg-light name col-md-6">
+									{{$order->user->contact->postalAddress1}}<br>
 									{{$order->user->contact->postalAddress2}}
 								</div>
-								<div class="billing-address bg-light">
-									<div class="row m-0">
-										<div class="col p-0 name">
+								<div class="row">
+									<div class="col-md-4">
+										<p class="name mb-0">City</p>
+										<div class="address-line bg-light name">
 											{{$order->user->contact->postalCity}}
 										</div>
-										<div class="col p-0 name">
+
+									</div>
+									<div class="col-md-4">
+										<P class="name mb-0">State</P>
+										<div class="address-line bg-light name">
 											{{$order->user->contact->postalState}}
 										</div>
-										<div class="col p-0 name">
-											{{$order->user->contact->postalPostCode}}
-										</div>
+
+									</div>
+									<div class="col-md-4 ">
+										<p class="name mb-0">Zip</p>
+										<div class="address-line bg-light name">
+											{{$order->user->contact->postalPostCode}}</div>
+
 									</div>
 								</div>
-
 							</div>
 						</div>
-
 						<div class="col-md-12">
 							<h5 class="fw-bold ps-1" style="font-family: 'Poppins'">Item Purchased</h5>
 						</div>
@@ -130,27 +150,27 @@
 					<div class="row">
 						<div class="col-md-12">
 							<table class="mb-5 thnak-you-page-top-section">
-								<tr>
+								<tr class="table-heading">
 									<th>Order number</th>
 									<th>Date</th>
 									<th>Email</th>
 									<th>phone</th>
 
 								</tr>
-								<tr>
+								<tr class="table-items">
 									<td>{{$order->id}}</td>
 									<td>{{$formatedDate}}</td>
 									<td>{{$order->user->email}}</td>
 									<td>{{$order->user->contact->phone}}</td>
 
 								</tr>
-								<tr>
+								<tr class="table-heading">
 									<th>Shipping</th>
 									<th>Text</th>
 									<th>Total</th>
 									<th>Payment Method</th>
 								</tr>
-								<tr>
+								<tr class="table-items">
 									<td></td>
 									<td></td>
 									<td>${{number_format($item->product->retail_price * $item->quantity, 2)}}</td>
@@ -164,4 +184,5 @@
 		</div>
 	</div>
 </div>
+@include('partials.product-footer')
 @include('partials.footer')
