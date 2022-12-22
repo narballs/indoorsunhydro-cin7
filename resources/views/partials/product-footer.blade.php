6c03eb9f0603de2c15235020125be09659c5235b
iqrariqqi741 <iqrariqqi741@gmail.com>
2022-12-22 18:29:20 +0500

    mobile view produt fillters

--- resources/views/partials/product-footer.blade.php
+++ resources/views/partials/product-footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="text-white text-lg-start bg-dark h-900 mt-5">
+<footer class="text-white text-lg-start bg-dark h-900 mt-5 desktop-view">
 	<div class="container-fluid">
 		<div class="row ">
 			<div class=" col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 text-dark ">
@@ -58,8 +58,124 @@
 			</div>
 			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
 				<p class="mt-4">
-					Copyright @ LA Garden Supply. All right reserved
+					Copyright @ Indoorsun Hydro. All right reserved
 				</p>
 			</div>
 		</div>
+</footer>
+
+<footer class="text-white text-lg-start bg-dark h-900 mt-5 mobile-view">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="main-page-footer-section footer-section-product">
+					<h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
+					<span>
+						<img src="/theme/img/map-pin.png" class="img-fluid ps-3">
+					</span>
+					<span class="ms-2 pra-footer">Los
+						Angeles
+						Warehouse
+						(HQ)
+					</span>
+					<p class="ms-4 ms-4-footer">&nbsp;
+						13310 S. Figueroa St. Los Angeles, CA 90061
+					</p>
+					<span>
+						<img src="/theme/img/phone-call.png" class="img-fluid ps-3">
+					</span>
+					<span class="ms-2 pra-footer">(916)
+						281-3090
+					</span>
+					<p class="ms-4 ms-4-footer">&nbsp;
+						<span class="text-success sm-4-footer">
+							MONDAY-SUNDAY 7 Dayes
+						</span> 9AM-5pm
+					</p>
+				</div>
+				<div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
+					<div class="d-flex">
+						<div class="justify-content-center aling-items-center">
+							<h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
+							<p class="card-text justify-content-end sm-4-footer">Returns</p>
+							<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+							<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+							<h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
+							<p class="card-text justify-content-end sm-4-footer">My orders</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product mt-5">
+					<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
+					<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
+				</div>
+			</div>
+			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+				<p class="mt-4">
+					Copyright @ Indoorsun Hydro. All right reserved
+				</p>
+			</div>
+		</div>
+	</div>
+</footer>
+
+<footer class="text-white text-lg-start bg-dark h-900 mt-5 ipad-view">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="main-page-footer-section footer-section-product">
+					<h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
+					<span>
+						<img src="/theme/img/map-pin.png" class="img-fluid ps-3">
+					</span>
+					<span class="ms-2 pra-footer">Los
+						Angeles
+						Warehouse
+						(HQ)
+					</span>
+					<p class="ms-4 ms-4-footer">&nbsp;
+						13310 S. Figueroa St. Los Angeles, CA 90061
+					</p>
+					<span>
+						<img src="/theme/img/phone-call.png" class="img-fluid ps-3">
+					</span>
+					<span class="ms-2 pra-footer">(916)
+						281-3090
+					</span>
+					<p class="ms-4 ms-4-footer">&nbsp;
+						<span class="text-success sm-4-footer">
+							MONDAY-SUNDAY 7 Dayes
+						</span> 9AM-5pm
+					</p>
+				</div>
+				<div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
+					<div class="d-flex">
+						<div class="justify-content-center aling-items-center">
+							<h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
+							<p class="card-text justify-content-end sm-4-footer">Returns</p>
+							<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+							<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+							<h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
+							<p class="card-text justify-content-end sm-4-footer">My orders</p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-12 main-page-footer-section footer-section-product mt-5">
+					<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
+					<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
+					<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
+				</div>
+				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+					<p class="mt-4">
+						Copyright @ Indoorsun Hydro. All right reserved
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </footer>
--- resources/views/products-by-brand.blade.php
+++ resources/views/products-by-brand.blade.php
@@ -7,7 +7,7 @@
    </p>
 </div>
 <?php //dd($products);?>
-<div class="container">
+<div class="container desktop-view">
    <form id="form-filter">
       <div class="col-md-12">
          <div class="row pl-5 pr-5 pb-4 pt-3" style="border: 1px solid rgba(0,0,0,.125);">
@@ -105,6 +105,138 @@
                      onclick="inStockOutstock('instock'), handleSelectChange()" value="{{$stock}}">{{$text}}</button>
 
 
+               </div>
+            </div>
+         </div>
+      </div>
+   </form>
+   <div class="row">
+      @foreach($products as $key=>$product)
+      @foreach($product->options as $option)
+      <div class="col-sm-12 col-md-6 col-lg-3 d-flex align-self-stretch">
+         <div class="card shadow-sm mb-4 w-100">
+            @if($option->image != '')
+            <a href="{{ url('product-detail/'.$product->id.'/'.$option->option_id) }}"><img src="{{$option['image']}}"
+                  class="col-md-10 offset-1" /></a>
+            @else
+            <img src="{{ asset('theme/img/image_not_available.png') }}" class="w-100 img-fluid h-75 w-75"
+               onclick="showdetails({{$product->id}})" />
+            @endif
+            <div class="card-body d-flex flex-column text-center">
+               <input type="hidden" name="quantity" value="1" id="quantity">
+               <input type="hidden" name="p_id" id="p_{{$product->id}}" value="{{$product->id}}">
+               <h5 class="card-title" style="font-weight: 500;
+                        font-size: 16px;"><a
+                     href="{{ url('product-detail/'.$product->id.'/'.$option->option_id.'/'.$product->slug) }}"
+                     id=product_name_{{$product->id}}>{{$product->name}}</a></h5>
+               <div class="mt-auto">
+                  <p class="text-uppercase mb-0 text-center text-danger">${{$product->retail_price}}</p>
+                  <button class="button-cards col w-100" style="max-height: 46px;"
+                     onclick="updateCart({{$product->id}},{{$option->option_id}})">Add to cart</button>
+               </div>
+            </div>
+         </div>
+      </div>
+      @endforeach
+      @endforeach
+   </div>
+</div>
+<div class="container mobile-view">
+   <form id="form-filter">
+      <div class="col-md-12">
+         <div class="row  pb-4 pt-3" style="border: 1px solid rgba(0,0,0,.125);">
+            <input type="hidden" id="category_id" value="{{ $category_id }}">
+            <input type="hidden" id="parent_category_slug" value="{{ $parent_category_slug }}">
+            <div class="col-md-12">
+               <label>Sort by</label>
+               <select class="form-select" id="search_price" onchange="handleSelectChange('best_selling')">
+                  <option value="0">Select Option</option>
+                  <option class="form-group" value="best-selling" {{ $price_creteria }} {{ isset($price_creteria) &&
+                     $price_creteria=='best-selling' ? 'selected="selected"' : '' }}>Best Selling</option>
+                  <option class="form-group" value="price-low-to-high" {{ $price_creteria }} {{ isset($price_creteria)
+                     && $price_creteria=='price-low-to-high' ? 'selected="selected"' : '' }}>Price Low to High</option>
+                  <option class="form-group" value="price-high-to-low" {{ $price_creteria }} {{ isset($price_creteria)
+                     && $price_creteria=='price-high-to-low' ? 'selected="selected"' : '' }}>Price High to Low</option>
+                  <option class="form-group" value="brand-a-to-z" {{ $price_creteria }} {{ isset($price_creteria) &&
+                     $price_creteria=='brand-a-to-z' ? 'selected="selected"' : '' }}>Product A to Z</option>
+                  <option class="form-group" value="brand-z-to-a" {{ $price_creteria }} {{ isset($price_creteria) &&
+                     $price_creteria=='brand-z-to-a' ? 'selected="selected"' : '' }}>Product Z to A</option>
+                  <option class="form-group" value="price">Best Selling</option>
+                  <option class="form-group" value="price-low-to-high" {{ $price_creteria }} {{ isset($price_creteria)
+                     && $price_creteria=='price-low-to-high' ? 'selected="selected"' : '' }}>Price Low to High</option>
+                  <option class="form-group" value="price-high-to-low" {{ $price_creteria }} {{ isset($price_creteria)
+                     && $price_creteria=='price-high-to-low' ? 'selected="selected"' : '' }}>Price High to Low</option>
+                  <option class="form-group" value="brand-a-to-z" {{ $price_creteria }} {{ isset($price_creteria) &&
+                     $price_creteria=='brand-a-to-z' ? 'selected="selected"' : '' }}>Product A to Z</option>
+                  <option class="form-group" value="brand-z-to-a" {{ $price_creteria }} {{ isset($price_creteria) &&
+                     $price_creteria=='brand-z-to-a' ? 'selected="selected"' : '' }}>Product Z to A</option>
+
+               </select>
+            </div>
+            <div class="col-md-12">
+               <?php //dd($category_id);?>
+               <label>Categories</label>
+
+               <select class="form-select" id="selected_cat" name="selected_cat"
+                  onchange="handleSelectChange('category')">
+                  <option>Select Category</option>
+                  @foreach($categories as $category)
+
+                  <option value="{{$category->id}}/{{$category->slug}}" {{ isset($category_id) &&
+                     $category_id==$category->id ? 'selected="selected"' : '' }}>{{ $category->name }}</option>
+                  <!-- <option value="{{$category->id}}/{{$category->slug}}" {{ isset($selected_category_id) && $selected_category_id == $selected_category_id ? 'selected="selected"' : '' }}>{{ $category->name }}</option> -->
+                  @endforeach
+
+                  <!-- @foreach($products as $key=>$product)
+                        <option value="{{$product->brand_id}}" >{{$product->brand}}</option>
+                  @endforeach -->
+               </select>
+            </div>
+
+            <div class="col-md-12">
+               <label>Brand</label>
+               <select class="form-select" id="brand" name="brands[]" onchange="handleSelectChange('brand')">
+                  <option>Select Brand</option>
+                  @foreach($brands as $_brand_id => $brand_name)
+
+                  <option value="{{ $_brand_id }}" {{ isset($brand_id) && $brand_id==$_brand_id ? 'selected="selected"'
+                     : '' }}>{{ $brand_name }}</option>
+                  @endforeach
+
+                  <!-- @foreach($products as $key=>$product)
+                        <option value="{{$product->brand_id}}" >{{$product->brand}}</option>
+                  @endforeach -->
+               </select>
+            </div>
+
+            <div class="col-md-12">
+               <label>Result per page</label>
+               <select id="per_page" class="form-select" onchange="handleSelectChange('result_per_page')">
+                  <option value="10" {{ $per_page }} {{ isset($per_page) && $per_page==10 ? 'selected="selected"' : ''
+                     }}>10</option>
+                  <option value="20" {{ $per_page }} {{ isset($per_page) && $per_page==20 ? 'selected="selected"' : ''
+                     }}>20</option>
+                  <option value="30" {{ $per_page }} {{ isset($per_page) && $per_page==30 ? 'selected="selected"' : ''
+                     }}>30</option>
+               </select>
+            </div>
+            <div class="col-md-12 d-flex mt-3">
+               <label>Show Only</label>
+
+               <div id="stock" style="padding-left: 139px;">
+                  <?php if(empty($stock) || $stock == 'in-stock') {
+                     $text = 'In stock';
+                     $danger = '';
+                     $stock = 'in-stock';
+                  }
+                  else {
+                     $text = 'Out of Stock';
+                     $danger = 'bg-danger';
+                     $stock = 'out-of-stock';
+                  }   
+                  ?>
+                  <button class="{{ $stock ? $stock : 'in-stock'  }} {{$danger}}" type="button" id="in-stock"
+                     onclick="inStockOutstock('instock'), handleSelectChange()" value="{{$stock}}">{{$text}}</button>
                </div>
             </div>
          </div>
