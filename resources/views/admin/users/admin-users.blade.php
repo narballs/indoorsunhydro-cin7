796944e8068dddff5e82bc72fba8ca2dc673682b
iqrariqqi741 <iqrariqqi741@gmail.com>
2023-02-17 13:01:09 +0500

    switch-user

--- resources/views/admin/users/admin-users.blade.php
+++ resources/views/admin/users/admin-users.blade.php
@@ -1,4 +1,3 @@
-
 @section('content')
 
 @if ($message = Session::get('success'))
@@ -7,41 +6,39 @@
 </div>
 @endif
 <table class="table table-bordered mt-5">
- <tr>
-   <th>No</th>
-   <th>Name</th>
-   <th>Email</th>
-   <th>Roles</th>
-   <th width="280px">Action</th>
- </tr>
+  <tr>
+    <th>No</th>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Roles</th>
+    <th width="280px">Action</th>
+  </tr>
 
- @foreach ($data as $key => $user)
+  @foreach ($data as $key => $user)
   <tr>
     <td>{{ ++$i }}</td>
-    <td>{{ $user->name }}</td>
+    <td>{{ $user->first_name }}</td>
     <td>{{ $user->email }}</td>
     <td>
       @if(!empty($user->getRoleNames()))
-        @foreach($user->getRoleNames() as $v)
-           <label class="badge badge-success">{{ $v }}</label>
-        @endforeach
+      @foreach($user->getRoleNames() as $v)
+      <label class="badge badge-success">{{ $v }}</label>
+      @endforeach
       @endif
     </td>
     <td>
-    
-            <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
-     
-        @can('user-edit')
-            <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-        @endcan
-        @can('user-delete')
-            {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-            {!! Form::close() !!}
-        @endcan
+
+      <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
+
+      @can('user-edit')
+      <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
+      @endcan
+      @can('user-delete')
+      {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
+      {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+      {!! Form::close() !!}
+      @endcan
     </td>
   </tr>
- @endforeach
+  @endforeach
 </table>
-
-
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,75 +7,82 @@
 @stop
 @section('content')
 <div class="row">
-    <div class="col-lg-12 margin-tb">
+    <div class="col-md-12 margin-tb">
         <div class="pull-left">
             <h2>Users Management</h2>
         </div>
-        <div class="pull-left d-flex justify-contend-end text-aling-end">
-            <a class="btn btn-primary" href="{{ route('users.create') }}"> Create New User</a>
+        <div class="row">
+            <div class="col-md-3">
+                <a class="btn btn-primary btn-sm" href="{{ route('users.create') }}"> Create New User</a>
+            </div>
+            {{-- <div class="col-md-3">
+                <a class="btn btn-info btn-sm" href="{{ route('users.create') }}"> Create New User</a>
+            </div> --}}
         </div>
+        {{-- <div class="pull-left d-flex justify-contend-end text-aling-end">
+
+        </div> --}}
     </div>
+
 </div>
 
 
 @if ($message = Session::get('success'))
 <div class="alert alert-success">
-  <p>{{ $message }}</p>
+    <p>{{ $message }}</p>
 </div>
 @endif
 
 <div class="row mt-2">
     <div class="col-md-12">
-       <span onclick="adminUsers()"> Admins({{$count}})</span>
+        <span onclick="adminUsers()"> Admins({{$count}})</span>
     </div>
-
 </div>
 <div id="admin-users"></div>
 <table class="table table-bordered mt-5" id="user-table">
- <tr>
-   <th>No</th>
-   <th>Name</th>
-   <th>Email</th>
-   <th>Roles</th>
-   <th width="280px">Action</th>
- </tr>
+    <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Roles</th>
+        <th width="280px">Action</th>
+    </tr>
 
- @foreach ($data as $key => $user)
-  <tr>
-    <td>{{ ++$i }}</td>
-    <td>{{ $user->name }}</td>
-    <td>{{ $user->email }}</td>
-    <td>
-      @if(!empty($user->getRoleNames()))
-        @foreach($user->getRoleNames() as $v)
-           <label class="badge badge-success">{{ $v }}</label>
-        @endforeach
-      @endif
-    </td>
-    <td>
-    
-            <a class="btn btn-info" href="{{ route('users.show',$user->id) }}">Show</a>
-     
-        @can('user-edit')
-            <a class="btn btn-primary" href="{{ route('users.edit',$user->id) }}">Edit</a>
-        @endcan
-        @can('user-delete')
+    @foreach ($data as $key => $user)
+    <tr>
+        <td>{{ ++$i }}</td>
+        <td>{{ $user->first_name }}</td>
+        <td>{{ $user->email }}</td>
+        <td>
+            @if(!empty($user->getRoleNames()))
+            @foreach($user->getRoleNames() as $v)
+            <label class="badge badge-success">{{ $v }}</label>
+            @endforeach
+            @endif
+        </td>
+        <td>
+            <a class="btn btn-info btn-sm" href="{{ route('users.show',$user->id) }}">Show</a>
+            @can('user-edit')
+            <a class="btn btn-primary btn-sm" href="{{ route('users.edit',$user->id) }}">Edit</a>
+            @endcan
+            @can('user-delete')
             {!! Form::open(['method' => 'DELETE','route' => ['users.destroy', $user->id],'style'=>'display:inline']) !!}
-                {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
             {!! Form::close() !!}
-        @endcan
-    </td>
-  </tr>
- @endforeach
+            @endcan
+            <a class="btn btn-success btn-sm" href="{{ url('admin/user-switch/'.$user->id) }}">Switch User</a>
+        </td>
+    </tr>
+    @endforeach
 </table>
 
 <div id="pageination">
-{!! $data->render() !!}
+    {!! $data->render() !!}
 </div>
 
 @endsection
 
- 
+
 <script>
     function adminUsers() {
         $('#user-table').addClass('d-none');
@@ -91,5 +98,3 @@
             });
     }
 </script>
-
- 
