9b98e7a72b14fd5e49e5cae1fcbcf6d86c67282d
waqas1981 <w.zeshan@tap.company>
2023-03-13 13:44:50 +0500

    User password reset

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,14 +17,15 @@ use App\Http\Requests\Users\UserSignUpRequest;
 use App\Http\Requests\Users\CompanyInfoRequest;
 use App\Jobs\SyncContacts;
 use Spatie\Permission\Models\Role;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\URL;
 use App\Helpers\MailHelper;
+use \Illuminate\Support\Str;
+use \Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -836,8 +837,18 @@ class UserController extends Controller
 
     public function send_password($id) {
         $user = User::where('id',$id)->first();
-        $plain_password = $user->new_password;
-            $data['email'] = $user->email;
+        $plain_password = Str::random(10) . date('YmdHis');
+        $encrypted_password = bcrypt($plain_password);
+        $hash = Str::random(10000) . $user->first_name . date('YmdHis');
+        $hash = md5($hash);
+
+        $user->password = $encrypted_password;
+        // $user->new_password = $plain_password;
+        $user->hash = $hash;
+        $user->save();
+
+        //$plain_password = $user->new_password;
+            $data['email'] = 'wqszeeshan@gmail.com';
             $data['content'] = 'Password Reset';
             $data['subject'] = 'Password Reset';
             $data['from'] = env('MAIL_FROM_ADDRESS');
