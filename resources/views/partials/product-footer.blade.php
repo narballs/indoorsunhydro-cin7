c03f3d0008a9c627f46f95f03639fa68f7d5dfc8
iqrariqqi741 <iqrariqqi741@gmail.com>
2023-04-12 15:49:43 +0500

    header-foooter-fix

--- resources/views/partials/product-footer.blade.php
+++ resources/views/partials/product-footer.blade.php
@@ -1,188 +1,188 @@
-<footer class="text-white text-lg-start bg-dark h-900 mt-5 desktop-view">
-	<div class="container-fluid">
-		<div class="row ">
-			<div class=" col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 text-dark ">
-				.
-			</div>
-			<div class="col-xl-3 col-lg-3 col-md-0 col-sm-0 col-xs-0 main-page-footer-section footer-section-product">
-				<h5 class="mb-4 contact-us-footer">CONTACT US</h5>
-				<span>
-					<img src="/theme/img/map-pin.png" class="img-fluid">
-				</span>
-				<span class="ms-2 pra-footer">5671 Warehouse Way
-				</span>
-				<p class="ms-4 mt-3 pra-footer">&nbsp;
-					{{-- 13310 S. Figueroa St. Los Angeles, CA 90061 --}}
-					Sacramento CA 95826
-				</p>
-				<span>
-					<img src="/theme/img/phone-call.png" class="img-fluid">
-				</span>
-				<span class="ms-2 pra-footer">(916)
-					281-3090
-				</span>
-				<p class="ms-4 ms-4-footer">&nbsp;
-					<span class="text-success sm-4-footer">
-						MONDAY-SUNDAY 7 Days
-					</span> 9AM-5pm
-				</p>
-				<p>
-					<a href=" https://www.yelp.com/biz/indoor-sun-hydro-sacramento-5?osq=indoorsun+hydro"
-						class="text-light pra-footer" style="text-decoration: none">
-						<img src="/theme/img/yelp_icons.png" alt="" class="img-fluid" style="width: 26px;">
+<footer class="text-white text-lg-start footer-container-product-footer h-900 mt-5 desktop-view">
+    <div class="container-fluid">
+        <div class="row ">
+            <div class=" col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 text-dark ">
+                .
+            </div>
+            <div class="col-xl-3 col-lg-3 col-md-0 col-sm-0 col-xs-0 main-page-footer-section footer-section-product">
+                <h5 class="mb-4 contact-us-footer">CONTACT US</h5>
+                <span>
+                    <img src="/theme/img/map-pin.png" class="img-fluid">
+                </span>
+                <span class="ms-2 pra-footer fw-semibold">5671 Warehouse Way
+                </span>
+                <p class="ms-4 mt-3 pra-footer">&nbsp;
+                    {{-- 13310 S. Figueroa St. Los Angeles, CA 90061 --}}
+                    Sacramento CA 95826
+                </p>
+                <span>
+                    <img src="/theme/img/phone-call.png" class="img-fluid">
+                </span>
+                <span class="ms-2 pra-footer fw-semibold">(916)
+                    281-3090
+                </span>
+                <p class="ms-4 ms-4-footer">&nbsp;
+                    <span class="text-success sm-4-footer">
+                        MONDAY-SUNDAY 7 Days
+                    </span> 9AM-5pm
+                </p>
+                <p class="social-media-icons-header-footer mb-0">social media</p>
+                <p>
+                    <a href=" https://www.yelp.com/biz/indoor-sun-hydro-sacramento-5?osq=indoorsun+hydro"
+                        class="text-light pra-footer" style="text-decoration: none">
+                        <img src="/theme/img/footer-yami-icon.png" alt="" class="img-fluid"
+                            style="width: 32px;">
 
-					</a>
-					<a href="https://www.instagram.com/indoorsunhydro/" class="text-light pra-footer"
-						style="text-decoration: none">
-						<img src="/theme/img/instagram_icons.png" alt="" class="ps-1" style="width: 32px;">
-					</a>
-				</p>
-				<p>
-
-				</p>
-			</div>
-			<div class="col-xl-1 col-lg-1 col-md-0 col-sm-0 col-xs-0 text-dark ">
-				.
-			</div>
-			<div
-				class="col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 mb-4 mb-md-0 main-page-footer-section footer-section-product">
-				<div class="d-flex">
-					<div class="justify-content-end">
-						<h5 class="text-uppercase mb-4 contact-us-footer">Customer Service</h5>
-						<p class="card-text justify-content-end sm-4-footer">Returns</p>
-						<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
-						<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
-						<h5 class="text-uppercase mb-4 contact-us-footer">MY ACCOUNTS</h5>
-						<p class="card-text justify-content-end sm-4-footer">My orders</p>
-					</div>
-				</div>
-			</div>
-			<div class="col-xl-1 col-lg-1 col-md- col-sm-0 col-xs-0 text-dark">
-				.
-			</div>
-			<div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product">
-				<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
-				<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
-				<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
-				<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
-				<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
-			</div>
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
-				<p class="mt-4">
-					Copyright @ Indoorsun Hydro. All right reserved
-				</p>
-			</div>
-		</div>
+                    </a>
+                    <a href="https://www.instagram.com/indoorsunhydro/" class="text-light pra-footer"
+                        style="text-decoration: none">
+                        <img src="/theme/img/footer-instagram-icon.png" alt="" class="ps-1"
+                            style="width: 38px;">
+                    </a>
+                </p>
+            </div>
+            <div class="col-xl-1 col-lg-1 col-md-0 col-sm-0 col-xs-0 text-dark ">
+                .
+            </div>
+            <div
+                class="col-xl-2 col-lg-2 col-md-0 col-sm-0 col-xs-0 mb-4 mb-md-0 main-page-footer-section footer-section-product">
+                <div class="d-flex">
+                    <div class="justify-content-end">
+                        <h5 class="text-uppercase mb-4 contact-us-footer">Customer Service</h5>
+                        <p class="card-text justify-content-end sm-4-footer">Returns</p>
+                        <p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+                        <p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+                        <h5 class="text-uppercase mb-4 contact-us-footer">MY ACCOUNTS</h5>
+                        <p class="card-text justify-content-end sm-4-footer">My orders</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-1 col-lg-1 col-md- col-sm-0 col-xs-0 text-dark">
+                .
+            </div>
+            <div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product">
+                <h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
+                <img class="img-fluid" src="/theme/img/paypal (2).png">
+                <img class="img-fluid" src="/theme/img/discover (2).png">
+                <img class="img-fluid" src="/theme/img/cours.png">
+                <img class="img-fluid" src="/theme/img/visa (2).png">
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+                <p class="mt-4">
+                    Copyright @ Indoorsun Hydro. All right reserved
+                </p>
+            </div>
+        </div>
 </footer>
 
 <footer class="text-white text-lg-start bg-dark h-90 mt-5 mobile-view">
-	<div class="container mt-5">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="main-page-footer-section footer-section-product">
-					<h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
-					<span>
-						<img src="/theme/img/map-pin.png" class="img-fluid ps-3">
-					</span>
-					<span class="ms-2 pra-footer">5671 Warehouse Way
-						Sacramento CA 95826
-					</span>
-					<p class="ms-4 ms-4-footer">&nbsp;
-						13310 S. Figueroa St. Los Angeles, CA 90061
-					</p>
-					<span>
-						<img src="/theme/img/phone-call.png" class="img-fluid ps-3">
-					</span>
-					<span class="ms-2 pra-footer">(916)
-						281-3090
-					</span>
-					<p class="ms-4 ms-4-footer">&nbsp;
-						<span class="text-success sm-4-footer">
-							MONDAY-SUNDAY
-						</span> 9AM-5pm
-					</p>
-				</div>
-				<div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
-					<div class="d-flex">
-						<div class="justify-content-center aling-items-center">
-							<h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
-							<p class="card-text justify-content-end sm-4-footer">Returns</p>
-							<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
-							<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
-							<h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
-							<p class="card-text justify-content-end sm-4-footer">My orders</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product mt-5">
-					<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
-					<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
-				</div>
-			</div>
-			<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
-				<p class="mt-4">
-					Copyright @ Indoorsun Hydro. All right reserved
-				</p>
-			</div>
-		</div>
-	</div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-page-footer-section footer-section-product">
+                    <h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
+                    <span>
+                        <img src="/theme/img/map-pin.png" class="img-fluid ps-3">
+                    </span>
+                    <span class="ms-2 pra-footer">5671 Warehouse Way
+                        Sacramento CA 95826
+                    </span>
+                    <p class="ms-4 ms-4-footer">&nbsp;
+                        13310 S. Figueroa St. Los Angeles, CA 90061
+                    </p>
+                    <span>
+                        <img src="/theme/img/phone-call.png" class="img-fluid ps-3">
+                    </span>
+                    <span class="ms-2 pra-footer">(916)
+                        281-3090
+                    </span>
+                    <p class="ms-4 ms-4-footer">&nbsp;
+                        <span class="text-success sm-4-footer">
+                            MONDAY-SUNDAY
+                        </span> 9AM-5pm
+                    </p>
+                </div>
+                <div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
+                    <div class="d-flex">
+                        <div class="justify-content-center aling-items-center">
+                            <h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
+                            <p class="card-text justify-content-end sm-4-footer">Returns</p>
+                            <p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+                            <p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+                            <h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
+                            <p class="card-text justify-content-end sm-4-footer">My orders</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 mb-4 mb-md-0 main-page-footer-section footer-section-product mt-5">
+                    <h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
+                    <img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                </div>
+            </div>
+            <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+                <p class="mt-4">
+                    Copyright @ Indoorsun Hydro. All right reserved
+                </p>
+            </div>
+        </div>
+    </div>
 </footer>
 
 <footer class="text-white text-lg-start bg-dark h-900 mt-5 ipad-view">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
-				<div class="main-page-footer-section footer-section-product">
-					<h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
-					<span>
-						<img src="/theme/img/map-pin.png" class="img-fluid ps-3">
-					</span>
-					<span class="ms-2 pra-footer">5671 Warehouse Way
-						Sacramento CA 95826
-					</span>
-					<p class="ms-4 ms-4-footer">&nbsp;
-						13310 S. Figueroa St. Los Angeles, CA 90061
-					</p>
-					<span>
-						<img src="/theme/img/phone-call.png" class="img-fluid ps-3">
-					</span>
-					<span class="ms-2 pra-footer">(916)
-						281-3090
-					</span>
-					<p class="ms-4 ms-4-footer">&nbsp;
-						<span class="text-success sm-4-footer">
-							MONDAY-SUNDAY
-						</span> 9AM-5pm
-					</p>
-				</div>
-				<div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
-					<div class="d-flex">
-						<div class="justify-content-center aling-items-center">
-							<h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
-							<p class="card-text justify-content-end sm-4-footer">Returns</p>
-							<p class="card-text justify-content-end sm-4-footer">Customer Support</p>
-							<p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
-							<h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
-							<p class="card-text justify-content-end sm-4-footer">My orders</p>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-12 main-page-footer-section footer-section-product mt-5">
-					<h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
-					<img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
-					<img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
-				</div>
-				<div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
-					<p class="mt-4">
-						Copyright @ Indoorsun Hydro. All right reserved
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-page-footer-section footer-section-product">
+                    <h5 class="contact-us-footer mt-3 ps-3">CONTACT US</h5>
+                    <span>
+                        <img src="/theme/img/map-pin.png" class="img-fluid ps-3">
+                    </span>
+                    <span class="ms-2 pra-footer">5671 Warehouse Way
+                        Sacramento CA 95826
+                    </span>
+                    <p class="ms-4 ms-4-footer">&nbsp;
+                        13310 S. Figueroa St. Los Angeles, CA 90061
+                    </p>
+                    <span>
+                        <img src="/theme/img/phone-call.png" class="img-fluid ps-3">
+                    </span>
+                    <span class="ms-2 pra-footer">(916)
+                        281-3090
+                    </span>
+                    <p class="ms-4 ms-4-footer">&nbsp;
+                        <span class="text-success sm-4-footer">
+                            MONDAY-SUNDAY
+                        </span> 9AM-5pm
+                    </p>
+                </div>
+                <div class="col-md-12 mt-3 main-page-footer-section footer-section-product">
+                    <div class="d-flex">
+                        <div class="justify-content-center aling-items-center">
+                            <h5 class="text-uppercase contact-us-footer mt-3">Customer Service</h5>
+                            <p class="card-text justify-content-end sm-4-footer">Returns</p>
+                            <p class="card-text justify-content-end sm-4-footer">Customer Support</p>
+                            <p class="card-text justify-content-end sm-4-footer">Hydro Guide and Tips</p>
+                            <h5 class="text-uppercase  contact-us-footer">MY ACCOUNTS</h5>
+                            <p class="card-text justify-content-end sm-4-footer">My orders</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12 main-page-footer-section footer-section-product mt-5">
+                    <h5 class=" text-uppercase mb-4 contact-us-footer"> PAYMENT METHOD</h5>
+                    <img class="img-fluid" src="/theme/img/paypal.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/discover.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/curus.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                    <img class="img-fluid" src="/theme/img/visa.png">&nbsp; &nbsp;&nbsp; &nbsp;
+                </div>
+                <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-xs-12 footer-end ">
+                    <p class="mt-4">
+                        Copyright @ Indoorsun Hydro. All right reserved
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
