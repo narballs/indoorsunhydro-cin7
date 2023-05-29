3eef5246b3ae5a8df1136bf48c149b11a7f4311e
iqrariqqi741 <iqrariqqi741@gmail.com>
2023-05-29 16:11:22 +0500

    supplier side fixed

--- resources/views/admin/contacts.blade.php
+++ resources/views/admin/contacts.blade.php
@@ -2,175 +2,229 @@
 @section('title', 'Dashboard')
 @section('content_header')
 @stop
-
 @section('content')
     <div class="table-wrapper">
-        <div class="card-body mt-2">
-            <div class="row mb-3">
+        <div class="card-body mt-2 product_secion_main_body">
+            <div class="row border-bottom product_section_header">
                 <div class="col-md-12">
                     <div class="row">
                         <div class="col-md-10">
-                            <p class="order_heading">
+                            <p class="product_heading">
                                 Suppliers
                             </p>
-                            <p class="order_description">
-                                In the Suppliers section, you can review and manage all suppliers with their details. You
-                                can view and edit information such as <br> supplier IDs, supplier name, address, contact
-                                information, and products supplied. Access to this area is restricted to<br> administrators
-                                and
-                                team leaders. Any changes you make will require approval after being verified for accuracy.
-                            </p>
                         </div>
-                        <div class="col-md-2">
-                            <button type="button" class="btn create_new_supplier_btn">
-                                + Create New Order
-                            </button>
+                        <div class="col-md-2 pt-3">
+                            <a href="#" class="btn create_new_shipping_btn">
+                                Create New Supplier +
+                            </a>
                         </div>
                     </div>
-                    <div class="row p-3 search_row_admin-interface">
-                        <div class="col-md-12 order-search">
-                            <div class="form-group has-search ">
+                    <div class="row search_row_admin-interface">
+                        <div class="col-md-2 product_search">
+                            <div class="has-search ">
                                 <span class="fa fa-search form-control-feedback"></span>
-                                <form method="get" action="/admin/contact" class="mb-2">
-                                    <input type="text" class="form-control border-0" id="search" name="search"
-                                        placeholder="Search for order ID, customer, order, status or something..."
-                                        value="{{ isset($search) ? $search : '' }}" />
+                                <form method="get" action="admin/admin/contacts" class="mb-2">
+                                    <input type="text" class="form-control" id="search" name="search"
+                                        placeholder="Search" value="{{ isset($search) ? $search : '' }}" />
                                 </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <table class="table  table-hover table-suppliers">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Name </th>
-                        <th>Status </th>
-                        <th>Price Tier</th>
-                        <th>Company </th>
-                        <th>Notes</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php //dd($orders)
-                    ?>
-                    @foreach ($contacts as $key => $contact)
-                        <tr id="row-{{ $contact->id }}" class="supplier-row">
-                            <td>{{ $key + 1 }}</td>
-                            <td class="supplier_name">
-                                <span>
-                                    {{ $contact->name }}
+            <div class="card-body product_table_body">
+                <table class="table border mb-5 table-contact-method">
+                    <thead>
+                        <tr class="table-header-background">
+                            <td class="d-flex table-row-item">
+                                <span class="tabel-checkbox">
+                                    <input type="checkbox" name="test" class="checkbox-table" id="selectAll">
+                                </span>
+                                <span class="table-row-heading">
+                                    <i class="fas fa-arrow-up"></i>
                                 </span>
                             </td>
-                            <td class="supplier-status">
-                                @if ($contact->status == '1')
-                                    <span class="badge badge-success w-100 supplier_status_1">Active</span>
-                                @else
-                                    <span class="badge badge-warning w-100 supplier_status_0">Inactive</span>
-                                @endif
+                            <td>
+                                <span class="d-flex table-row-item">Name</span>
                             </td>
-                            <td>{{ $contact->price_tier }}</td>
-                            <td>{{ $contact->company }}</td>
-                            <td>{{ $contact->notes }}</td>
-                            <td class="supplier_action">
-                                <a href="{{ url('admin/order-detail/' . $contact->id) }}" class="view a_class"
-                                    title="" data-toggle="tooltip" data-original-title="View">
-                                    <i class="icon-style  fas fa-eye fa-border i_class"></i>
-                                </a>
-                                <a href="#" class="edit a_class" title="" data-toggle="tooltip"
-                                    data-original-title="Edit"><i class="icon-style fas fa-edit fa-border "></i>
-                                </a>
-                                <a href="#" class="delete a_class" title="" data-toggle="tooltip"
-                                    data-original-title="Delete"><i class="icon-style fas fa-trash-alt fa-border "></i>
-                                </a>
+                            <td>
+                                <span class="d-flex table-row-item">Status</span>
+                            </td>
+                            <td>
+                                <span class="d-flex table-row-item">Price Tier</span>
+                            </td>
+                            <td>
+                                <span class="d-flex table-row-item">Company</span>
+                            </td>
+                            <td>
+                                <span class="d-flex table-row-item">Notes</span>
+                            </td>
+                            <td>
+                                <span class="d-flex table-row-item">Action</span>
+                            </td>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($contacts as $key => $contact)
+                            <tr id="row-{{ $contact->id }}" class="supplier-row border-bottom">
+                                <td class="d-flex supplier-table-items">
+                                    <span class="tabel-checkbox">
+                                        <input type="checkbox" name="test" class="checkbox-table">
+                                    </span>
+                                    <span class="table-row-heading">
+                                        {{ $key + 1 }}
+                                    </span>
+                                </td>
+                                <td class="supplier_name pb-0 pt-3">
+                                    <span>
+                                        {{ $contact->firstName }}
+                                    </span>
+                                </td>
+                                <td class="supplier-status pb-0 pt-3">
+                                    @if ($contact->status == '1')
+                                        <span class="badge badge-success  supplier_status_1">Active</span>
+                                    @else
+                                        <span class="badge badge-warning supplier_status_0">Inactive</span>
+                                    @endif
+                                </td>
+                                <td class="pb-0 pt-3">
+                                    @if (!empty($contact->priceColunm))
+                                        {{ $contact->priceColumn }}
+                                    @else
+                                        <span class="badge badge-danger supplier_status_2">empty</span>
+                                    @endif
+                                </td>
+                                <td class="pb-0 pt-3">
+                                    <span>
+                                        {{ $contact->company }}
+                                    </span>
+                                </td>
+                                <td class="pb-0 pt-3">
+                                    <span>
+                                        {{ $contact->notes }}
+                                    </span>
+                                </td>
+                                <td class="created_by toggleClass">
+                                    <div class="btn-group">
+                                        <button type="button" class="btn btn-white dropdown-toggle" data-toggle="dropdown"
+                                            aria-haspopup="true" aria-expanded="false">
+                                            <i class="fas fa-ellipsis-h" style="color: #CBCBCB !important;"></i>
+                                        </button>
+                                        <div class="dropdown-menu dropdonwn_menu">
+                                            <a class="dropdown-item" href="{{ url('admin/order-detail/' . $contact->id) }}"
+                                                class="view a_class" title="" data-toggle="tooltip"
+                                                data-original-title="View">Previews
+                                            </a>
+                                            <a class="dropdown-item"href="#" class="edit a_class" title=""
+                                                data-toggle="tooltip" data-original-title="Edit">Edit
+                                            </a>
+                                            <a class="dropdown-item delete deleteIcon a_class" href="#" class=""
+                                                id="{{ $contact->id }}" title="" data-toggle="tooltip"
+                                                data-original-title="Delete">Delete
+                                            </a>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colspan="7">
+                                {{ $contacts->links('pagination.custom_pagination') }}
                             </td>
                         </tr>
-                    @endforeach
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colspan="7">
-                            {{ $contacts->links('pagination.custom_pagination') }}
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
+                    </tfoot>
+                </table>
+            </div>
         </div>
-    </div>
-@stop
+    @stop
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-    <link rel="stylesheet" href="{{ asset('admin/admin_lte.css') }}">
-    <style>
-        .text-successs {
-            color: #7CC633 !important;
-            font-family: 'Poppins', sans-serif !important;
-        }
+    @section('css')
+        <link rel="stylesheet" href="/css/admin_custom.css">
+        <link rel="stylesheet" href="{{ asset('admin/admin_lte.css') }}">
+        <style type="text/css">
+            .text-successs {
+                color: #7CC633 !important;
+                font-family: 'Poppins', sans-serif !important;
+            }
 
-        .badge-success {
-            background: rgba(124, 198, 51, 0.2);
-            color: #7CC633 !important;
-            padding: 7px !important;
-        }
+            .badge-success {
+                background: rgb(186 235 137 / 20%);
+                color: #319701;
+                padding: 6px !important;
+                font-style: normal;
+                font-weight: 500;
+                font-size: 11.3289px;
 
-        .badge-warning {
-            color: #1f2d3d;
-            background-color: #fce9a9;
-            color: #ffc107 !important;
-            padding: 5px;
-        }
+            }
 
-        .badge-danger {
-            color: #fff;
-            background-color: #f1abb2;
-            color: #f14f4f;
-            padding: 6px !important;
-        }
-    </style>
-@stop
+            .badge-warning {
+                background-color: #f1e8cb;
+                color: #b58903 !important;
+                padding: 6px !important;
+                font-style: normal;
+                font-weight: 500;
+                font-size: 11.3289px;
+            }
 
-@section('js')
-    <script>
-        $('.supplier-row').hover(function() {
-            let id = $(this).attr('id');
-            children = $(this).children('.supplier_name').children('span').addClass('text-successs');
+            .badge-danger {
+                color: #fff;
+                background-color: rgba(220, 78, 65, 0.12);
+                color: #DC4E41;
+                padding: 6px !important;
+                font-style: normal;
+                font-weight: 500;
+                font-size: 11.3289px;
+            }
+        </style>
+    @stop
+
+    @section('js')
+        <script>
+            $('.supplier-row-none').hover(function() {
+                let id = $(this).attr('id');
+                children = $(this).children('.supplier_name').children('span').addClass('text-successs');
 
 
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_1').addClass(
-                'background-success');
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_0').addClass(
-                'background-warning');
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_2').addClass(
-                'background-danger');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_1').addClass(
+                    'background-success');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_0').addClass(
+                    'background-warning');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_2').addClass(
+                    'background-danger');
 
 
-            let tet = $(this).children('.supplier_action').children('a');
-            let get_class = tet.each(function(index, value) {
-                let test = tet[index].children[0];
-                test.classList.add('bg-icon');
+                let tet = $(this).children('.supplier_action').children('a');
+                let get_class = tet.each(function(index, value) {
+                    let test = tet[index].children[0];
+                    test.classList.add('bg-icon');
+                });
             });
-        });
 
 
-        $('.supplier-row').mouseleave(function() {
-            let id = $(this).attr('id');
-            children = $(this).children('.supplier_name').children('span').removeClass('text-successs');
+            $('.supplier-row-none').mouseleave(function() {
+                let id = $(this).attr('id');
+                children = $(this).children('.supplier_name').children('span').removeClass('text-successs');
 
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_1').removeClass(
-                'background-success');
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_0').removeClass(
-                'background-warning');
-            bg_success = $(this).children('.supplier-status').children('.supplier_status_2').removeClass(
-                'background-danger');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_1').removeClass(
+                    'background-success');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_0').removeClass(
+                    'background-warning');
+                bg_success = $(this).children('.supplier-status').children('.supplier_status_2').removeClass(
+                    'background-danger');
 
-            let tet = $(this).children('.supplier_action').children('a');
-            let get_class = tet.each(function(index, value) {
-                let test = tet[index].children[0];
-                test.classList.remove('bg-icon');
+                let tet = $(this).children('.supplier_action').children('a');
+                let get_class = tet.each(function(index, value) {
+                    let test = tet[index].children[0];
+                    test.classList.remove('bg-icon');
+                });
             });
-        });
-    </script>
-@stop
+
+            $(document).on('click', '#selectAll', function(e) {
+                var table = $(e.target).closest('table');
+                $('td input:checkbox', table).prop('checked', this.checked);
+            });
+        </script>
+    @stop
