763d0bcdfbeb2b2df17581c713618e94afd77e5d
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-06-05 17:27:22 +0500

    update total and price

--- resources/views/products-by-brand.blade.php
+++ resources/views/products-by-brand.blade.php
@@ -521,6 +521,7 @@
                      $('.cartQtyipad').html(total_cart_quantity);
                      $('.cartQtymbl').html(total_cart_quantity);
                      $('#topbar_cart_total').html('$'+parseFloat(cart_total).toFixed(2));
+                     $('.topbar_cart_total_ipad').html('$'+parseFloat(cart_total).toFixed(2));
                     var total = document.getElementById('#top_cart_quantity');
                }});
 
