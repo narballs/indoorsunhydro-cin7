7293b87efad562b301496a5b2bb078d49ec603bb
iqrariqqi741 <iqrariqqi741@gmail.com>
2023-06-07 13:13:50 +0500

    admin interface order page change icons

--- config/adminlte.php
+++ config/adminlte.php
@@ -230,20 +230,14 @@ return [
         ],
 
         // Sidebar items:
-        [
-            'type' => 'sidebar-menu-search',
-            'text' => 'search',
-        ],
+
         [
             'text' => 'blog',
             'url'  => 'admin/blog',
             'can'  => 'manage-blog',
         ],
 
-        [
-            'text' => 'profile',
-            'url'  => 'admin/settings',
-        ],
+
         [
             'text' => 'Orders',
             'url'  => 'admin/orders',
