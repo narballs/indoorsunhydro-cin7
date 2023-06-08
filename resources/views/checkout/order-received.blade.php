494e71591d685aa9d7d0375e518cbd57f92ce073
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-06-08 13:49:59 +0500

    thankyou and product detail page add to cart fixed

--- resources/views/checkout/order-received.blade.php
+++ resources/views/checkout/order-received.blade.php
@@ -46,6 +46,35 @@
 			margin-left: 0px !important;
 		}
 	}
+
+	@media  screen and (min-width : 360px ) and (max-width:425px) { 
+		.m_chechout_image {
+			width: 57.08px !important;
+			height: 57.08px !important;
+		}
+		.purchaseTable {
+			margin: 0rem !important;
+    		padding: 0rem !important;
+		}
+		.for_desktop {
+			display:none;
+		}
+		.for_mobile {
+			display:block !important;
+		}
+		.innerTable_mobile,tr, td, th {
+			border:0px !important;
+		}
+		.border_bottom_mb {
+			border-bottom:1px solid #DFDFDF !important;
+		}
+		.mobile_b_bottom {
+			border-bottom:1px solid #DFDFDF !important;
+		}
+		.order-confirmation-page-view-invoice-button {
+			margin-top: 10px !important;
+		}
+	}
 	@media  screen and (min-width : 768px ) and (max-width:1405px) {
 		.media-1440 {
 			display:none;
@@ -90,11 +119,11 @@
 	}
 	@media  screen and (min-width : 375px ) and (max-width:700px) {
 		.div_increase_mobile  {
-			margin-left:0px !important;
-			margin-right:0px !important;
-			padding: 0px !important;
-			padding-left:0px !important;
-			padding-right:0px !important;
+			margin-left:0rem !important;
+			margin-right:0rem !important;
+			padding: 0rem !important;
+			padding-left:0rem !important;
+			padding-right:0rem !important;
 		}
 
 		.add_border {
@@ -111,29 +140,66 @@
 			padding-left: 0px !important;
 		}
 
+		.main-thankyou-div {
+			width:100% !important;
+			padding: 0px !important;
+			margin: 0px !important;
+		}
+		.thank-you-card-body {
+			padding-left: 0.1rem !important;
+			padding-right: 0.1rem !important;
+			margin-top: 0rem !important;
+		}
+		.thank-you-card {
+			margin-top: 0px !important;
+		}
+		.thank-you-title-top {
+			margin-top: 0px !important;
+		}
+		.order-confirmation-page-title {
+			margin-bottom: 0px !important;
+		}
 		
-	}
+		.shipping_mobile {
+			margin-top: 2rem !important;
+		}
+		.order-confirmation-page-shipping-address {
+			margin-bottom: 0px !important;
+			padding-bottom: 20px !important;
+		}
+		.order-confirmation-page-billing-address {
+			margin-bottom: 0px !important;
+			padding-bottom: 20px !important;
+		}
 
-	@media  screen and (min-width : 375px ) and (max-width:500px) {
-		.mobile_text_class {
-			padding-left:1.8rem !important;
+		.div_increase_mobile  {
+			margin-left:0rem !important;
+			margin-right:0.5rem !important;
+			padding: 0rem !important;
+			padding-left:0rem !important;
+			padding-right:0rem !important;
+		}
+
+		.order-confirmation-page-second-row {
+			padding-top: 2rem !important;
 		}
 		
 	}
+
 	
 </style>
 {{session()->forget('cart');}}
 
-<div class="container-fluid" style="width:89%;">
-	<div class="row">
+<div class="container-fluid main-thankyou-div" style="width:89%;">
+	<div class="">
 		<div class="col-md-12">
-			<div class="card mt-5 border-0">
-				<div class="card-body ps-5 mt-5 ">
+			<div class="card mt-5 border-0 thank-you-card">
+				<div class="card-body ps-5 mt-5  thank-you-card-body">
 					<div class="row ps-5 mobile_class">
 						<div class=" col-xl-12 col-lg-12 col-md-12 col-sm-12">
 							<p class="order-confirmation-page-top-heading mobile-font">Order Confirmation</p>
 						</div>
-						<div class="col-md-12 mt-4">
+						<div class="col-md-12 mt-4 thank-you-title-top">
 							<p class="order-confirmation-page-title">
 								{{$order_contact->firstName}}
 								{{$order_contact->lastName}}
@@ -323,7 +389,7 @@
 					{{-- for media screen 768 above end --}}
 
 					{{-- for media screen mobile --}}
-					<div class="row ms-5 p-4 me-5 order-confirmation-page-invoice-row media_mobile div_increase_mobile add_border" style="padding-top: 50px !important;">
+					<div class="row ms-5 p-4 me-5 order-confirmation-page-invoice-row media_mobile div_increase_mobile add_border">
 						<div class="col-sm-12">
 							<div class="row">
 								<div class="col-sm-6">
@@ -395,8 +461,7 @@
 					{{-- for media mobile  end --}}
 					<div class="row">
 						<div class="col-md-12">
-							<div class="row  ms-5 p-4 me-5 order-confirmation-page-second-row div_increase_mobile"
-								style="padding-top: 5rem!important;">
+							<div class="row  ms-5 p-4 me-5 order-confirmation-page-second-row div_increase_mobile" style="padding-top: 5rem!important;">
 								<div class="col-md-6">
 									<p class="order-confirmation-page-billing-address mobile-font">
 										Billing Address
@@ -421,7 +486,22 @@
 														{{$order_contact->address1}}
 													</p>
 													@endif
-													<div class="row mt-4">
+
+													<p class="order-confirmation-page-address-line-tow-title">
+														Address line 2
+													</p>
+													@if ($order_contact->address2)
+													<p class="order-confirmation-page-address-line-tow-item">
+														{{$order_contact->address2}}
+													</p>
+													@endif
+													<p class="order-confirmation-page-address-line-tow-item">
+														Addressline2 empty
+													</p>
+													
+												</div>
+												<div class="col-md-6">
+													<div class="row">
 														<div class="col-xl-4 col-lg-6 col-md-12 col-sm-12">
 															<p class="order-confirmation-page-city-name-title">City</p>
 															@if (!$order_contact->city)
@@ -463,24 +543,11 @@
 														</div>
 													</div>
 												</div>
-												<div class="col-md-6">
-													<p class="order-confirmation-page-address-line-tow-title">
-														Address line 1
-													</p>
-													@if ($order_contact->address2)
-													<p class="order-confirmation-page-address-line-tow-item">
-														{{$order_contact->address2}}
-													</p>
-													@endif
-													<p class="order-confirmation-page-address-line-tow-item">
-														Addressline empty
-													</p>
-												</div>
 											</div>
 										</div>
 									</div>
 								</div>
-								<div class="col-md-6">
+								<div class="col-md-6 shipping_mobile">
 									<p class="order-confirmation-page-shipping-address mobile-font">
 										Shipping Address
 									</p>
@@ -568,10 +635,10 @@
 						</div>
 					</div>
 					<div class="row ps-5 mt-5 pe-5 div_increase_mobile">
-						<div class="col-md-12 mt-5">
-							<p class="order-confirmation-page-item-purchased-title mobile-font">Item Purchased </p>
+						<div class="col-md-12 mt-5 mobile_b_bottom">
+							<p class="order-confirmation-page-item-purchased-title mobile-font ">Item Purchased </p>
 						</div>
-						<div class="col-md-12 mt-5">
+						<div class="col-md-12 mt-5 for_desktop">
 							<table class="table">
 								<tr>
 									<th class="order-confirmation-page-table-data-heading">Name</th>
@@ -587,9 +654,9 @@
 									<tr>
 										<td>
 											<div class="row">
-												<div class="col-md-2 py-2">
+												<div class="col-md-2 py-2 mobile_thankyou_img_div">
 													@if ($option->image)
-													<img class="img-fluid img-thumbnail" src="{{$option->image}}" alt=""
+													<img class="img-fluid img-thumbnail m_chechout_image" src="{{$option->image}}" alt=""
 														width="90px" style="max-height: 90px">
 													@else
 													<img src="/theme/img/image_not_available.png" alt="" width="80px">
@@ -633,244 +700,117 @@
 								</div>
 							</div>
 						</div>
+
+						{{-- data show for mobile --}}
+						<div class="col-md-12 mt-5 purchaseTable for_mobile d-none">
+							<table class="table main_table_mobile">
+								{{-- <tr>
+									<th class="order-confirmation-page-table-data-heading">Image</th>
+									<th class="order-confirmation-page-table-data-heading"
+										style="padding-left: 0px; !important">Name</th>
+										<th class="order-confirmation-page-table-data-heading"
+										style="padding-left: 0px; !important">
+										Quantity</th>
+									<th class="order-confirmation-page-table-data-heading">Price</th>
+								</tr> --}}
+								<tbody class="border-0">
+									@foreach($order->apiOrderItem as $item)
+									
+										@foreach($item->product->options as $option)
+										
+										<tr class="border_bottom_mb">
+											<td style="width: 20% !important;">
+												{{-- <div class="row">
+													<div class="col-md-2 py-2 mobile_thankyou_img_div">
+														@if ($option->image)
+														<img class="img-fluid img-thumbnail m_chechout_image" src="{{$option->image}}" alt=""
+															width="90px" style="max-height: 90px">
+														@else
+														<img src="/theme/img/image_not_available.png" class="m_chechout_image" alt="" width="80px">
+														@endif
+													</div>
+													<div class="col-md-5 py-2 ps-0 prod-name-img mobile_text_class" style="">
+														<a class="order-confirmation-page-product-category-name pb-3"
+															href=" {{ url('product-detail/'. $item->product->id.'/'.$option->option_id.'/'.$item->product->slug) }}">
+															{{$item->product->name}}
+														</a>
+														<br>
+														<p class="order-confirmation-page-product-title">Title:<span
+																class="order-confirmation-page-product-item">
+																{{$item->product->name}}</span>
+														</p>
+													</div>
+												</div> --}}
+												<div class="py-2 mobile_thankyou_img_div">
+													@if ($option->image)
+													<img class="img-fluid img-thumbnail m_chechout_image" src="{{$option->image}}" alt=""
+														width="90px" style="max-height: 90px">
+													@else
+													<img src="/theme/img/image_not_available.png" class="m_chechout_image" alt="" width="80px">
+													@endif
+												</div>
+											</td>
+											<td style="width:80%;">
+												<div class="ps-0 mobile_text_class" style="">
+													<a class="order-confirmation-page-product-category-name pb-3"
+														href=" {{ url('product-detail/'. $item->product->id.'/'.$option->option_id.'/'.$item->product->slug) }}">
+														{{$item->product->name}}
+													</a>
+													<br>
+													<p class="order-confirmation-page-product-title">Title:<span
+															class="order-confirmation-page-product-item">
+															{{$item->product->name}}</span>
+													</p>
+												</div>
+												<table border="0" style="width:100%" cellpadding="0" cellspacing="0" class="innerTable_mobile">
+													<tr>
+														<td style="text-align: right">
+															<p class=" mb-0 order-confirmation-page-product-price">
+																${{number_format($item->price,2)}}</p>
+														</td>
+													</tr>
+												</table>
+											</td>
+											{{-- <td>Shipping</td> --}}
+											{{-- <td>
+												<div class="row">
+													<div class="col-md-12">
+														<p class="pt-4 order-confirmation-page-product-quantity">
+															{{$item->quantity}}</p>
+													</div>
+												</div>
+											</td>
+											<td>
+												<p class="pt-4 order-confirmation-page-product-price">
+													${{number_format($item->price,2)}}</p>
+											</td> --}}
+											{{-- <td >
+												<p class="pt-4 order-confirmation-page-product-quantity">
+													{{$item->quantity}}</p>
+											</td>
+											<td >
+												<p class="pt-4 order-confirmation-page-product-price">
+													${{number_format($item->price,2)}}</p>
+											</td> --}}
+										</tr>
+										@endforeach
+									@endforeach
+								</tbody>
+							</table>
+							<div class="row">
+								<div class="col-md-2 m-auto">
+									<a href="#" class="order-confirmation-page-view-invoice-button btn w-100">VIEW
+										INVOICE</a>
+								</div>
+							</div>
+						</div>
+						{{-- data show for mobile end --}}
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
-{{-- <div class="container mt-5 ">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="card  border-0">
-				<div class="card-header bg-white border-0 mt-3">
-					<div class="row">
-						<div class="col-md-4">
-							<p class="fw-bold fs-1 order-page-order-confiremd">Order Confirmed.</p>
-						</div>
-						<div class="col-md-6 mt-3 ps-5 pe-2">
-							<p class="order-page-user-name"> <span><img src="/theme/img/star.png" alt=""></span> Thank
-								You
-								<span class="fw-bold">
-									{{$order->user->first_name}} {{$order->user->last_name}},
-								</span>
-								Your Order has been received!
-							</p>
-						</div>
-						<div class="col-md-1 pb-2">
-							<img src="/theme/img/crose.png" alt="">
-						</div>
-					</div>
-				</div>
-				<div class="card-body">
-					<div class="row">
-						<div class="col-md-6">
-							<div class="billing-address bg-light p-3">
-								<div class="bg-light">
-									<div class="billing-address-heading">Billing Address</div>
-									<div class="row mt-2">
-										<div class="col-md-6 name mt-4">
-											<div class="address-line bg-light mb-1">
-												Your Name
-											</div>
-											{{$order_contact->firstName}}
-											{{$order_contact->lastName}}
-										</div>
-										<div class="col-md-6 name mt-4">
-											<div class="address-line bg-light mb-1">
-												Your Company Name
-											</div>
-											{{$order_contact->company}}
-										</div>
-									</div>
-								</div>
-								<div class="address-line bg-light mt-2">
-									Address line 1
-								</div>
-
-								<div class="bg-light name">
-									{{$order_contact->postalAddress1}}
-								</div>
-								<div class="address-line bg-light">
-									Address line 2
-								</div>
-								<div class="bg-light name">
-									{{$order_contact->postalAddress2}}
-								</div>
-								<div class="row m-0 bg-light">
-									<div class="col p-0 address-line">
-										City
-									</div>
-									<div class="col p-0 address-line">
-										State
-									</div>
-									<div class=" col p-0 address-line">
-										Zip
-									</div>
-								</div>
-								<div class="billing-address bg-light">
-									<div class="row m-0">
-										<div class="col p-0 name">
-											{{$order_contact->postalCity}}
-										</div>
-										<div class="col p-0 name">
-											{{$order_contact->postalState}}
-										</div>
-										<div class="col p-0 name">
-											{{$order_contact->postalPostCode}}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-6">
-							<div class="billing-address bg-light p-3">
-								<div class="bg-light">
-									<div class="shipping-address-heading">Shipping Address</div>
-									<div class="row mt-2">
-										<div class="col-md-6 name mt-4">
-											<div class="address-line bg-light mb-1">
-												My Name
-											</div>
-											{{$order_contact->firstName}}
-											{{$order_contact->lastName}}
-										</div>
-										<div class="col-md-6 name mt-4">
-											<div class="address-line bg-light mb-1">
-												My Company Name
-											</div>
-											{{$order_contact->company}}
-
-										</div>
-									</div>
-								</div>
-								<div class="address-line bg-light mt-2">
-									Address line 1
-								</div>
-
-								<div class="bg-light name">
-									{{$order_contact->postalAddress1}}
-								</div>
-								<div class="address-line bg-light">
-									Address line 2
-								</div>
-								<div class="bg-light name">
-									{{$order_contact->postalAddress2}}
-								</div>
-								<div class="row m-0 bg-light">
-									<div class="col p-0 address-line">
-										City
-									</div>
-									<div class="col p-0 address-line">
-										State
-									</div>
-									<div class=" col p-0 address-line">
-										Zip
-									</div>
-								</div>
-								<div class="billing-address bg-light">
-									<div class="row m-0">
-										<div class="col p-0 name">
-											{{$order_contact->postalCity}}
-										</div>
-										<div class="col p-0 name">
-											{{$order_contact->postalState}}
-										</div>
-										<div class="col p-0 name">
-											{{$order_contact->postalPostCode}}
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-md-12">
-							<h5 class="fw-bold ps-1 item-purchased">Item Purchased
-							</h5>
-						</div>
-						@foreach($order->apiOrderItem as $item)
-						@foreach($item->product->options as $option)
-						{{
-						if($pricing == 'WholesaleUSD') {
-						$retail_prices = $option->wholesalePrice;
-						}
-						else {
-						$retail_prices = $option->retailPrice;
-						}
-						}}
-
-						<div class="row ps-5 mt-3">
-							<div class="col-md-2">
-								@if($option->image)
-								<div class="mt-4 order-page-prdoct-img ps-1 pt-1 pb-1">
-									<img class="order-page-product-image" src="{{ $option->image}}" alt=""
-										style="    background: #fff !important;">
-								</div>
-								@else
-								<div class="mt-4 order-page-prdoct-img ps-1 pt-1 pb-1">
-									<img class="order-page-product-image" src="/theme/img/image_not_available.png"
-										alt="" style="    background: #fff !important;">
-								</div>
-								@endif
-							</div>
-							<div class=" col-md-6 mt-5">
-								<a class="thnak-you-page-product-name"
-									href="{{ url('product-detail/'. $item->product->id.'/'.$option->option_id.'/'.$item->product->slug) }}">
-									{{$item->product->name}}
-								</a>
-							</div>
-							<div class="col-md-2 mt-5">
-								<p class="d-flex justify-content-between align-items-center order-page-prduct-quantity">
-									{{$item->quantity}}</p>
-							</div>
-							<div class="col-md-2 mt-5">
-								<p class="thank-you-page-product-price">${{
-									number_format($retail_prices,2)}}</p>
-							</div>
-						</div>
-						@endforeach
-						@endforeach
-					</div>
-				</div>
-				<card-footer class="ps-5 mt-5">
-					<div class="row">
-						<div class="col-md-12">
-							<table class="mb-5 thnak-you-page-top-section">
-								<tr class="table-heading">
-									<td class="table-heading-subheading">Order number</td>
-									<td class="table-heading-subheading">Date</td>
-									<td class="table-heading-subheading">Email</td>
-									<td class="table-heading-subheading">phone</td>
-								</tr>
-								<tr class="table-items">
-									<td class="table-items-sub-item">{{$order->id}}</td>
-									<td class="table-items-sub-item">{{$formatedDate}}</td>
-									<td class="table-items-sub-item">{{$order->user->email}}</td>
-									<td class="table-items-sub-item">{{$order_contact->phone}}</td>
-								</tr>
-								<tr class="table-heading">
-									<td class="table-heading-subheading">Shipping</td>
-									<td class="table-heading-subheading">Tax</td>
-									<td class="table-heading-subheading">Total</td>
-									<td class="table-heading-subheading">Payment Method</td>
-								</tr>
-								<tr class="table-items">
-									<td class="table-items-sub-item">$</td>
-									<td class="table-items-sub-item">$</td>
-									<td class="table-items-sub-item">${{number_format($order->total, 2)}}</td>
-									<td class="table-items-sub-item">{{$order->paymentTerms}}</td>
-
-								</tr>
-							</table>
-							<div class="col-md-12 justify-content-center align-items-center d-flex">
-								<button class="view-invoice mt-5" type="button">View invoice</button>
-							</div>
-						</div>
-					</div>
-				</card-footer>
-			</div>
-		</div>
-	</div>
-</div> --}}
 
 @include('partials.product-footer')
 @include('partials.footer')
