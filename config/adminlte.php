c77b7c77b11a72f8be613644bb8e1257e03d1e47
iqrariqqi741 <iqrariqqi741@gmail.com>
2023-06-14 13:01:07 +0500

    bugs fixed

--- config/adminlte.php
+++ config/adminlte.php
@@ -291,15 +291,15 @@ return [
 
     'menu' => [
         // Navbar items:
-        [
-            'type'         => 'navbar-search',
-            'text'         => 'search',
-            'topnav_right' => true,
-        ],
-        [
-            'type'         => 'fullscreen-widget',
-            'topnav_right' => true,
-        ],
+        // [
+        //     'type'         => 'navbar-search',
+        //     'text'         => 'search',
+        //     'topnav_right' => true,
+        // ],
+        // [
+        //     'type'         => 'fullscreen-widget',
+        //     'topnav_right' => true,
+        // ],
 
         // Sidebar items:
 
