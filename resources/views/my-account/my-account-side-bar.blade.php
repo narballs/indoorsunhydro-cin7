855215d8a39b6f4564b8d5615f3b5465eaede46e
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-07-26 15:07:26 +0500

    my-account-moblile

--- resources/views/my-account/my-account-side-bar.blade.php
+++ resources/views/my-account/my-account-side-bar.blade.php
@@ -1,18 +1,18 @@
 <ul class="nav order_detail_tabs">
-    <li class="text-center" style="width: 109px !important ;">
-        <a class="nav-link order_left_border @yield('order-active')" href="{{ url('my-account') }}">Orders</a>
+    <li class="text-center my-account-list-items-li" style="width: 109px;">
+        <a class="nav-link my-account-list-items order_left_border @yield('order-active')" href="{{ url('my-account') }}">Orders</a>
     </li>
-    <li class="text-center" style="width: 109px !important ;">
-        <a class="nav-link  @yield('account-active')" href="{{ route('account_profile') }}">Profile</a>
+    <li class="text-center my-account-list-items-li" style="width: 109px;">
+        <a class="nav-link my-account-list-items   @yield('account-active')" href="{{ route('account_profile') }}">Profile</a>
     </li>
-    <li class="text-center" style="width: 123px !important ;">
-        <a class="nav-link @yield('addresses-active')" href="{{ route('my_account_address') }}">Addresses</a>
+    <li class="text-center my-account-list-items-li" style="width: 123px;">
+        <a class="nav-link my-account-list-items  @yield('addresses-active')" href="{{ route('my_account_address') }}">Addresses</a>
     </li>
-    <li class="text-center" style="width: 109px !important ;">
-        <a class="nav-link @yield('my-favorites-active')" href="{{ route('my_favorites') }}">Favorites</a>
+    <li class="text-center my-account-list-items-li" style="width: 109px;">
+        <a class="nav-link my-account-list-items  @yield('my-favorites-active')" href="{{ route('my_favorites') }}">Favorites</a>
     </li>
 
-    <li class="text-center" style="width: 109px !important ;">
-        <a class="nav-link @yield('users-active')" href="{{ route('additional_users') }}">Users</a>
+    <li class="text-center my-account-list-items-li" style="width: 109px;">
+        <a class="nav-link my-account-list-items  @yield('users-active')" href="{{ route('additional_users') }}">Users</a>
     </li>
 </ul>
