bdc03e4bbef1e224445ce2a79d5e2dc590c6ee15
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-08-17 11:14:22 +0500

    fixed tax class issue with new user registered

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -696,16 +696,16 @@ class UserController extends Controller
                     ]);
                 }
                 
-                // Auth::loginUsingId($user->id);
-                // $companies = Contact::where('user_id', auth()->user()->id)->get();
-                // if ($companies->count() == 1) {
-                //     if ($companies[0]->contact_id == null) {
-                //         UserHelper::switch_company($companies[0]->secondary_id);
-                //     } else {
-                //         UserHelper::switch_company($companies[0]->contact_id);
-                //     }
-                // }
-                // Session::put('companies', $companies);
+                Auth::loginUsingId($user->id);
+                $companies = Contact::where('user_id', auth()->user()->id)->get();
+                if ($companies->count() == 1) {
+                    if ($companies[0]->contact_id == null) {
+                        UserHelper::switch_company($companies[0]->secondary_id);
+                    } else {
+                        UserHelper::switch_company($companies[0]->contact_id);
+                    }
+                }
+                Session::put('companies', $companies);
             }
         }
 
