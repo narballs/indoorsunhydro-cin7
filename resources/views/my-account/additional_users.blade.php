00c8a5700a8a536ff4e636f1253550f3245179df
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-08-17 13:00:21 +0500

    adjustments for my account

--- resources/views/my-account/additional_users.blade.php
+++ resources/views/my-account/additional_users.blade.php
@@ -44,13 +44,13 @@
                                     <thead class="border-bottom-0 border-bottom-0">
                                         <tr class="table-header-background">
                                             <td class="table-row-item">
-                                                <div class="custom-control custom-checkbox tabel-checkbox">
+                                                <div class="custom-control custom-checkbox tabel-checkbox d-flex align-items-center">
                                                     <input
                                                         class="custom-control-input custom-control-input-success checkbox-table"
                                                         type="checkbox" id="selectAll" value="">
                                                     <label for="selectAll" class="custom-control-label"></label>
                                                     <span class="table-row-heading-order">
-                                                        <i class="fas fa-arrow-up mt-1" style="font-size:14.5px ;"></i>
+                                                        <i class="fas fa-arrow-up" style="font-size:14.5px ;"></i>
                                                     </span>
                                                 </div>
                                             </td>
@@ -70,7 +70,7 @@
                                         <?php //dd($contact);?>
                                             <tr>
                                                 <td class="table-items justify-content-start align-items-lg-center user_table_items">
-                                                    <div class="custom-control custom-checkbox tabel-checkbox">
+                                                    <div class="custom-control custom-checkbox tabel-checkbox d-flex align-items-center">
                                                         <input
                                                             class="custom-control-input custom-control-input-success sub_chk"
                                                             data-id="{{ $contact->id }}" type="checkbox"
