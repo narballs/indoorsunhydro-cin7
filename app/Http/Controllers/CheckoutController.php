7ce43e1ea9ace29d592a3fd47c1c7f7506899516
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-08-22 18:38:55 +0500

    added test function to capture event

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -245,7 +245,7 @@ class CheckoutController extends Controller
                 $parent_email = Contact::where('contact_id', $active_contact_id)->first();
         
                 $data['subject'] = 'Your order has been received';
-                $data['email'] = $email;
+                $data['email'] = 'engrdanishsabir00@gmail.com';
                 MailHelper::sendMailNotification('emails.admin-order-received', $data);
         
             
