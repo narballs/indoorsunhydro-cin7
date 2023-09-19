66ea0bb977069a5ef2a6e5b10d0edc72cc82ed2a
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-09-19 17:48:45 +0500

    added collapse and company column

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1195,7 +1195,13 @@ class UserController extends Controller
         $user = User::where('id', $user_id)->first();
         $all_ids = UserHelper::getAllMemberIds($user);
         $user_address = Contact::where('user_id', $user_id)->first();
-        $secondary_contacts = Contact::whereIn('id', $all_ids)->paginate(10);
+        $all_companies = Contact::whereIn('id', $all_ids)->groupBy('company')->get();
+        // $secondary_contacts = null;
+        // if (count($all_companies) > 0) {
+        //     foreach ($all_companies as $company) {
+        //         $secondary_contacts = Contact::where('company', $company->company)->paginate(10);
+        //     }
+        // }
         $list = BuyList::where('id', 20)->with('list_products.product.options')->first();
         $contact = Contact::where('email', $user_address->email)->first();
         $companies = Contact::where('user_id', $user_id)->get();
@@ -1246,11 +1252,12 @@ class UserController extends Controller
             'lists',
             'user',
             'user_address',
-            'secondary_contacts',
+            // 'secondary_contacts',
             'parent',
             'companies',
             'states',
-            'contact_id'
+            'contact_id',
+            'all_companies'
         ));
     }
 
