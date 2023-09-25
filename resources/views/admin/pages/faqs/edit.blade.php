4c843e2bcfc284ba1e1e81dd6a2f13e6b2bd7207
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-09-25 20:58:10 +0500

    making correction

--- resources/views/admin/pages/faqs/edit.blade.php
+++ resources/views/admin/pages/faqs/edit.blade.php
@@ -5,7 +5,7 @@
 @section('content_header')
 <div class="row ">
     <div class="col-md-6">
-        <h1>Edit Page</h1>
+        <h1>Edit Faq</h1>
     </div>
     <div class="col-md-6 text-right">
         <a class="btn btn-primary text-white" href="{{ route('faqs.index') }}">Back</a>
