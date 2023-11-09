12c80832885f0a3f035f58135b0ade3710ab9607
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-11-09 12:04:13 +0500

    fixing address issues

--- resources/views/checkout/order-received.blade.php
+++ resources/views/checkout/order-received.blade.php
@@ -883,7 +883,7 @@
 														{{$order_contact->postalAddress1 ? $order_contact->postalAddress1 . ',' : $order_contact->address1 . ','}}
 													</p>
 													<p class="order-confirmation-page-address-line-one-title mb-1">
-														{{$order_contact->postalAddress2 ? $order_contact->postalAddress2: $order_contact->address2 }}
+														{{$order_contact->postalAddress2 ? $order_contact->postalAddress2: $order_contact->address2 . ','}}
 													</p>
 													<p class="order-confirmation-page-address-line-one-title">
 														{{$order_contact->postalCity ? $order_contact->postalCity . ',' : $order_contact->city . ','}}
