a7323881b551883f71080f9b984502a8b6141958
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-12-04 14:35:10 +0500

    added grey and timestap for deleted users

---
+++ database/migrations/2023_12_03_233422_add_is_deleted_to_contacts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsDeletedToContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            $table->timestamp('is_deleted')->nullable()->after('notes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('contacts', function (Blueprint $table) {
+            //
+        });
+    }
+}
