98c449526aa0c859fdea7d9ea40f9639a9a7ea85
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2023-12-22 01:22:07 +0500

    added blog

--- resources/views/partials/blogs_partial.blade.php
+++ resources/views/partials/blogs_partial.blade.php
@@ -12,7 +12,7 @@
 						<a href="{{route('blog_detail' , $blog->slug)}}"><img class="card-img-top rounded-top" alt="100%x280"  src="{{asset('pages/blogs/thumbnails/' . $blog->thumbnail)}}" alt="Blog Image"></a>
 						@endif
 						<div class="card-body blog-card-body-height">
-							<h5 class="card-title">{{$blog->title}}</h5>
+							<h5 class="card-title">{!! \Illuminate\Support\Str::limit($blog->title, 25) !!}</h5>
 							<p class="card-text mb-0">{!! \Illuminate\Support\Str::limit($blog->description, 130) !!}</p>
 							<div class="row justify-content-between">
 								<div class="col-md-5">
