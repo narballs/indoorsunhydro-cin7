fd8d1e023d64007b6c6792554efc1a9de80d69fd
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-01-02 07:53:31 +0500

    removed gtin

--- app/Http/Controllers/GoogleContentController.php
+++ app/Http/Controllers/GoogleContentController.php
@@ -128,7 +128,7 @@ class GoogleContentController extends Controller
                 $product->setCondition($add_product['condition']);
                 $product->setBrand($add_product['brand']);
                 $product->setGoogleProductCategory($add_product['google_product_category']);
-                $product->setGtin('9780007350896');
+                // $product->setGtin('9780007350896');
                 $product->setAgeGroup('adult');
                 $product->setColor('universal');
                 $product->setGender('unisex');
