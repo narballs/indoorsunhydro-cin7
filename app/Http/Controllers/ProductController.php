e5e4d11140cc4d3b9f95133e76412642c0bb6771
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-01-08 15:32:37 +0500

    added pimages table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -27,8 +27,11 @@ use App\Models\User;
 
 use App\Helpers\UtilHelper;
 use App\Models\ApiOrderItem;
+use App\Models\ProductImage;
 use App\Models\ProductStock;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Http;
 
 use Illuminate\Database\Eloquent\Builder;
 
@@ -195,6 +198,110 @@ class ProductController extends Controller
 
     public function showAllProducts(Request $request)
     {
+        
+        $all_products = Product::with('options','options.defaultPrice', 'product_brand', 'categories' , 'product_views','apiorderItem', 'product_stock')
+        ->with(['product_views','apiorderItem' , 'options' => function ($q) {
+            $q->where('status', '!=', 'Disabled');
+            
+        }])
+        ->whereHas('options.defaultPrice', function ($q) {
+            $q->where('retailUSD', '!=', 0);
+        })
+        ->whereHas('categories' , function ($q) {
+            $q->where('is_active', 1);
+        })
+        ->where('status' , '!=' , 'Inactive')
+        ->where('barcode' , '!=' , '')
+        ->get();
+        $image = '';
+        if (count($all_products) > 0) {
+            foreach ($all_products as $product) {
+                if (count($product->options) > 0) {
+                    foreach ($product->options as $option) {
+                        $product_images = ProductImage::where('product_id', $product->id)->first();
+                        if (!empty($product_images)) {
+
+                            if (!empty($product->images)) {
+                                $client = new Client();
+                                $response = $client->get($image);
+                                $imageData = $response->getBody()->getContents();
+                                if ($response->getStatusCode() == 200) {
+                                    $publicPath = public_path('/theme/products/images/');
+                                    if (!file_exists($publicPath)) {
+                                        mkdir($publicPath, 0777, true);
+                                    }
+                                    $imageName = time() . '_' . uniqid() . '.png';
+                                    $imagePath = $publicPath . $imageName;
+                                    file_put_contents($imagePath, $imageData);
+
+                                    $product_images->image = $imageName;
+                                    $product_images->save();
+                                } else {
+                                    $sourcePath = asset('/theme/img/image_not_available.png');
+                                    $imageName = time() . '_' . uniqid() . '.' . pathinfo($sourcePath, PATHINFO_EXTENSION);
+                                    $destinationPath = public_path('/theme/products/images/');
+                                    // Copy the file to the new location
+                                    copy($sourcePath, $destinationPath . $imageName);
+                                    $product_images->image = $imageName;
+                                    $product_images->save();
+                                }
+                            } else {
+                                $sourcePath = asset('/theme/img/image_not_available.png');
+                                $imageName = time() . '_' . uniqid() . '.' . pathinfo($sourcePath, PATHINFO_EXTENSION);
+                                $destinationPath = public_path('/theme/products/images/');
+                                // Copy the file to the new location
+                                copy($sourcePath, $destinationPath . $imageName);
+                                $product_images->image = $imageName;
+                                $product_images->save();
+                            }
+                        } else {
+                            if (!empty($product->images)) {
+                                $client = new Client();
+                                $response = $client->get($image);
+                                $imageData = $response->getBody()->getContents();
+                                if ($response->getStatusCode() == 200) {
+                                    $publicPath = public_path('/theme/products/images/');
+                                    if (!file_exists($publicPath)) {
+                                        mkdir($publicPath, 0777, true);
+                                    }
+                                    $imageName = time() . '_' . uniqid() . '.png';
+                                    $imagePath = $publicPath . $imageName;
+                                    file_put_contents($imagePath, $imageData);
+
+                                    $productImages = new ProductImage();
+                                    $productImages->product_id = $product->id;
+                                    $productImages->image = $imageName;
+                                    $productImages->save();
+                                }
+                                else {
+                                    $sourcePath = asset('/theme/img/image_not_available.png');
+                                    $imageName = time() . '_' . uniqid() . '.' . pathinfo($sourcePath, PATHINFO_EXTENSION);
+                                    $destinationPath = public_path('/theme/products/images/');
+                                    // Copy the file to the new location
+                                    copy($sourcePath, $destinationPath . $imageName);
+                                    $productImages = new ProductImage();
+                                    $productImages->product_id = $product->id;
+                                    $productImages->image = $imageName;
+                                    $productImages->save();
+                                }
+                                
+                                
+                            } else {
+                                $sourcePath = asset('/theme/img/image_not_available.png');
+                                $imageName = time() . '_' . uniqid() . '.' . pathinfo($sourcePath, PATHINFO_EXTENSION);
+                                $destinationPath = public_path('/theme/products/images/');
+                                // Copy the file to the new location
+                                copy($sourcePath, $destinationPath . $imageName);
+                                $product_images = new ProductImage();
+                                $product_images->product_id = $product->id;
+                                $product_images->image = $imageName;
+                                $product_images->save();
+                            }
+                        }
+                    }
+                }
+            }
+        }
         if ($request->get('per_page')) {
             $per_page = $request->get('per_page');
         } else {
