5d2b651fc250e5d87039616dcfa08f4e45295907
xiaawan <xiaawan@users.noreply.github.com>
2024-01-12 18:54:31 +0500

    product search based on weight

--- app/Http/Controllers/Admin/AdminProductController.php
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -18,13 +18,15 @@ class AdminProductController extends Controller
 
     }
     
-    public function index(Request $request) {
+    public function index_backup(Request $request) {
         $search = $request->get('search');
+        $do_search = $request->get('do_search');
         $weight_filter_type = $request->get('weight_filter_type');
         $weight_value = $request->get('weight_value');
         $price_filter_type = $request->get('price_filter_type');
         $price_value = $request->get('price_value');
         $products = Product::with('categories', 'options')->paginate(10);
+        
         if(isset($search)) {
             $products = Product::with('categories', 'options')->where('name', 'LIKE', '%' . $search . '%')
             ->orWhere('code', 'like', '%' . $search . '%')
@@ -32,22 +34,23 @@ class AdminProductController extends Controller
             ->orWhere('retail_price', 'like', '%' . $search . '%')
             ->paginate(10);
         }
+
         if (isset($weight_filter_type) && isset($weight_value)) {
-            if($weight_filter_type == 'greater_then') {
+            if ($weight_filter_type == 'greater_than') {
                 $products = Product::with('categories' , 'options')
                     ->whereHas('options' , function($query) use ($weight_value){
                         $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
                 })
                 ->where('status'  , '!=' , 'Inactive')
                 ->paginate(10);
-            } else if($weight_filter_type == 'less_then') {
+            } elseif($weight_filter_type == 'less_than') {
                 $products = Product::with('categories' , 'options')
                     ->whereHas('options' , function($query) use ($weight_value){
                         $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
                 })
                 ->where('status'  , '!=' , 'Inactive')
                 ->paginate(10);
-            } else if($weight_filter_type == 'equal_to') {
+            } elseif($weight_filter_type == 'equal_to') {
                 $products = Product::with('categories' , 'options')
                     ->whereHas('options' , function($query) use ($weight_value){
                         $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
@@ -58,8 +61,130 @@ class AdminProductController extends Controller
         }
        
         
-        return view('admin/products', compact('products' , 'search'));
+        return view('admin/products', compact(
+            'products', 
+            'search',
+            'do_search',
+            'weight_filter_type',
+            'weight_value',
+            'price_filter_type',
+            'price_value'
+        ));
+    }
 
+    public function index(Request $request) {
+        $search = $request->get('search');
+        $do_search = $request->get('do_search');
+        $weight_filter_type = $request->get('weight_filter_type');
+        $weight_value = $request->get('weight_value');
+        $price_filter_type = $request->get('price_filter_type');
+        $price_value = $request->get('price_value');
+        $products_query = Product::with('categories', 'options');
+        
+        if(isset($search)) {
+            $products_query->where('name', 'LIKE', '%' . $search . '%')
+            ->orWhere('code', 'like', '%' . $search . '%')
+            ->orWhere('status', 'like', '%' . $search . '%')
+            ->orWhere('retail_price', 'like', '%' . $search . '%');
+        }
+
+        if (isset($weight_filter_type) && isset($weight_value)) {
+            $operation = '';
+
+            if ($weight_filter_type == 'equal_to') {
+                $operation = '=';
+            } elseif($weight_filter_type == 'less_than') {
+                $operation = '<';
+            } elseif($weight_filter_type == 'greater_than') {
+                $operation = '>';
+            }
+
+            $products_query->whereHas('options' , function($query) use ($weight_value, $operation){
+                $query->where('optionWeight', $operation, $weight_value)->where('status', '!=', 'disabled');
+            });
+        }
+
+        // if (isset($weight_filter_type) && isset($weight_value)) {
+        //     if ($weight_filter_type == 'greater_than') {
+        //         $products = Product::with('categories' , 'options')
+        //             ->whereHas('options' , function($query) use ($weight_value){
+        //                 $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
+        //         })
+        //         ->where('status'  , '!=' , 'Inactive')
+        //         ->paginate(10);
+        //     } elseif($weight_filter_type == 'less_than') {
+        //         $products = Product::with('categories' , 'options')
+        //             ->whereHas('options' , function($query) use ($weight_value){
+        //                 $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
+        //         })
+        //         ->where('status'  , '!=' , 'Inactive')
+        //         ->paginate(10);
+        //     } elseif($weight_filter_type == 'equal_to') {
+        //         $products = Product::with('categories' , 'options')
+        //             ->whereHas('options' , function($query) use ($weight_value){
+        //                 $query->where('optionWeight', '>', $weight_value)->where('status', '!=', 'disabled');
+        //         })
+        //         ->where('status'  , '!=' , 'Inactive')
+        //         ->paginate(10);
+        //     }
+        // }
+
+        
+        
+        $download_csv = $request->get('download_csv');
+        if ($download_csv == '1') {
+            $products = $products_query->get();
+            if (!empty($products)) {
+
+                $csv_data = [];
+                $csv_data[] = [
+                    'Product Name',
+                    'Product Code',
+                    'Product Status',
+                    'Product Retail Price',
+                    'Product Weight',
+                ];
+
+                foreach ($products as $product) {
+                    $csv_data[] = [
+                        $product->name,
+                        $product->code,
+                        $product->status,
+                        $product->retail_price,
+                        isset($product->options) ? $product->options[0]->optionWeight : '',
+                    ];
+                }
+
+                $csv_file_name = 'products.csv';
+                $file_path = public_path($csv_file_name);
+                $file = fopen($file_path, 'w');
+                foreach ($csv_data as $line) {
+                    fputcsv($file, $line);
+                }
+                fclose($file);
+
+                $headers = array(
+                    'Content-Type' => 'text/csv',
+                );
+
+                return response()->download($file_path, $csv_file_name, $headers);
+            }
+
+        }
+        else {
+            $products = $products_query->paginate(10)->appends(request()->query());
+        }
+       
+        
+        return view('admin/products', compact(
+            'products', 
+            'search',
+            'do_search',
+            'weight_filter_type',
+            'weight_value',
+            'price_filter_type',
+            'price_value'
+        ));
     }
 
     public function show($id) {
