eaf9b279c7b5d554825611ea12b8597ffa0a6fa7
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-01-22 11:37:22 +0500

    user notification template

---
+++ resources/views/emails/user_registration_notification.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>{{ \App\Helpers\SettingHelper::getSetting('website_name') }}</title>
+</head>
+<body>
+    <p>Hi {{name}},</p>
+    <p>
+        {{$content}} <a href="{{url('admin/users')}}">{{ \App\Helpers\SettingHelper::getSetting('website_name') }}</a>.
+    </p>
+    <p>
+        <strong>Name:</strong> {{ $name}}<br>
+        <strong>Email:</strong> {{ $email }}<br>
+    </p>
+</body>
+</html>
