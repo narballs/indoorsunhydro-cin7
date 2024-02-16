a000f4bc0ec20d8fe9394bddaf876a1e15db0bfa
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-02-16 14:35:51 +0500

    added images according to option

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -126,13 +126,46 @@
                                             </div>
                                             <div class="col-md-6">
                                                 @if (!empty($productOption->option1) || !empty($productOption->option2) || !empty($productOption->option3))
+                                                    @php
+                                                        $image_src = [];
+                                                        $options_array = ['option1', 'option2', 'option3'];
+                                                        $image_type_array = ['case' => 'case.png', 'pallet' => 'pallet.png', 'box' => 'box.png' , 'pack' => 'case.png' , 'pk' =>'case.png'];
+                                                        foreach ($options_array as $option) {
+                                                            foreach ($image_type_array as $key => $image_type) {
+                                                                if (strpos($productOption[$option], $key) !== false) {
+                                                                    $image_src[$option] = $image_type;
+                                                                }
+                                                            }
+                                                        }
+
+                                                    @endphp
                                                     <div class="row align-items-center">
                                                         <div class="col-md-2">
-                                                            <img src="/theme/img/box_icon.png" style="max-width: 40px;" />
+                                                            @if (isset($image_src['option1']))
+                                                                <img src= "{{asset('theme/bootstrap5/images/' . $image_src['option1'] )}}" style="max-width: 40px;" />
+                                                            @endif
                                                         </div>
                                                         <div class="col-md-10">
                                                         <p class="mb-0">{{ $productOption->option1 }}</p>
-                                                        <p class="mb-0">{{ $productOption->option2 }}</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-2">
+                                                            @if (isset($image_src['option2']))
+                                                                <img src= "{{asset('theme/bootstrap5/images/' . $image_src['option2'] )}}" style="max-width: 40px;" />
+                                                            @endif
+                                                        </div>
+                                                        <div class="col-md-10">
+                                                        <p class="mb-0">{{ $productOption->option2 }}</p> 
+                                                        </div>
+                                                    </div>
+                                                    <div class="row align-items-center">
+                                                        <div class="col-md-2">
+                                                            @if (isset($image_src['option3']))
+                                                                <img src= "{{asset('theme/bootstrap5/images/' . $image_src['option3'] )}}" style="max-width: 40px;" />
+                                                            @endif
+                                                        </div>
+                                                        <div class="col-md-10">
                                                         <p class="mb-0">{{ $productOption->option3 }}</p> 
                                                         </div>
                                                     </div>
