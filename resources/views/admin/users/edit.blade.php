c2837c459d4f03f6978623f52c14962be982574c
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-03-18 11:20:35 +0500

    added autocoimplete off

--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -36,7 +36,7 @@
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Email:</strong>
-                {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control']) !!}
+                {!! Form::text('email', null, ['placeholder' => 'Email', 'class' => 'form-control' , 'autocomplete' => 'off']) !!}
             </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12">
