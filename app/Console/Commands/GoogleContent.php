e3b8260e39dfa20e98869443f7d95f4649b091bf
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-03-20 15:14:26 +0500

    updated command for gmc

--- app/Console/Commands/GoogleContent.php
+++ app/Console/Commands/GoogleContent.php
@@ -82,101 +82,185 @@ class GoogleContent extends Command
      * @param array $token
      * @return mixed
      */
-    private function insertProducts($client, $token)
+    private function insertProducts($token , $client)
     {
+        
         $price_column = null;
         $default_price_column = AdminSetting::where('option_name', 'default_price_column')->first();
         if (!empty($default_price_column)) {
             $price_column = $default_price_column->option_value;
-        } else {
+        }
+        else {
             $price_column = 'retailUSD';
         }
-
         $product_array = [];
         $product_categories = Category::where('is_active', 1)->pluck('category_id')->toArray();
-        $all_products_ids = Product::whereIn('category_id', $product_categories)->pluck('product_id')->toArray();
-        $product_options_ids = ProductOption::whereIn('product_id', $all_products_ids)
-            ->where('status', '!=', 'Disabled')
-            ->where('optionWeight', '>', 0)
-            ->pluck('option_id')->toArray();
-        $product_pricing_option_ids = Pricingnew::whereIn('option_id', $product_options_ids)
-            ->where($price_column, '>', 0)
-            ->pluck('option_id')->toArray();
-        $products_ids = ProductOption::whereIn('option_id', $product_pricing_option_ids)->pluck('product_id')->toArray();
-
-        $products = Product::with('options', 'options.defaultPrice', 'product_brand', 'product_image', 'categories')
-            ->whereIn('product_id', $products_ids)
-            ->where('status', '!=', 'Inactive')
-            ->where('barcode', '!=', '')
-            ->get();
-
-        if ($products->isNotEmpty()) {
+        $all_products_ids = Product::whereIn('category_id' , $product_categories)
+        ->pluck('product_id')->toArray();
+        $product_options_ids = ProductOption::whereIn('product_id' , $all_products_ids)
+        ->where('status', '!=', 'Disabled')
+        ->where('optionWeight', '>', 0)
+        ->pluck('option_id')->toArray();
+        $product_pricing_option_ids = Pricingnew::whereIn('option_id' , $product_options_ids)
+        ->where($price_column , '>' , 0)
+        ->pluck('option_id')
+        ->toArray();
+        $products_ids = ProductOption::whereIn('option_id' , $product_pricing_option_ids)
+        ->pluck('product_id')->toArray();
+        $products = Product::with('options','options.defaultPrice','product_brand','product_image','categories')->whereIn('product_id' , $products_ids)
+        ->where('status' , '!=' , 'Inactive')
+        ->where('barcode' , '!=' , '')
+        ->get();
+        if (count($products) > 0) {
             foreach ($products as $product) {
-                foreach ($product->options as $option) {
-                    $category = 'General > General';
-                    if ($product->categories) {
-                        if ($product->categories->parent_id == 0) {
-                            $category = $product->categories->category_id;
-                        } elseif ($product->categories->parent_id != 0) {
-                            $category = $product->categories->parent_id;
+                
+                if (count($product->options) > 0) {
+                    foreach ($product->options as $option) {
+                        $category = 'General > General';
+                        if (!empty($product->categories)) {
+                            if (!empty($product->categories->category_id) && $product->categories->parent_id == 0) {
+                                $category = $product->categories->category_id;
+                            } else if (!empty($product->categories->parent_id) && !empty($product->categories->category_id) && $product->categories->parent_id != 0) {
+                                $category = $product->categories->parent_id;
+                            }
                         }
+                        else {
+                            $category = 'General > General';
+                        }
+                        
+                        $product_array[] = [
+                            'id' => $product->id,
+                            'title' => $product->name,
+                            'code' => $product->code,
+                            'description' => !empty($product->description) ? strip_tags($product->description) : 'No description available',
+                            'link' => url('product-detail/' . $product->id . '/' . $option->option_id . '/' . $product->slug),
+                            'image_link' => !empty($product->product_image->image) ? url(asset('theme/products/images/' . $product->product_image->image)) : url(asset('theme/img/image_not_available.png')),
+                            'price' => !empty($option->price[0]->$price_column) ? $option->price[0]->$price_column : 0,
+                            'condition' => 'new',
+                            'availability' => !empty($option) && $option->stockAvailable > 0 ? 'In stock' : 'Out of stock',
+                            'brand' => !empty($product->product_brand->name) ? $product->product_brand->name : 'General brand',
+                            'barcode' => $product->barcode,
+                            'google_product_category' => $category,
+                            'product_weight' => $option->optionWeight,
+                        ];
                     }
-
-                    $product_array[] = [
-                        'id' => $product->id,
-                        'title' => $product->name,
-                        'code' => $product->code,
-                        'description' => $product->description ? strip_tags($product->description) : 'No description available',
-                        'link' => url('product-detail/' . $product->id . '/' . $option->option_id . '/' . $product->slug),
-                        'image_link' => $product->product_image->image ? url(asset('theme/products/images/' . $product->product_image->image)) : url(asset('theme/img/image_not_available.png')),
-                        'price' => $option->price[0]->$price_column ?? 0,
-                        'condition' => 'new',
-                        'availability' => $option->stockAvailable > 0 ? 'In stock' : 'Out of stock',
-                        'brand' => $product->product_brand->name ?? 'General brand',
-                        'barcode' => $product->barcode,
-                        'google_product_category' => $category,
-                        'product_weight' => $option->optionWeight,
-                    ];
                 }
             }
+        }
+        
+        // $chunks = array_chunk($product_array, 100);
+        $client->setAccessToken($token['access_token']); // Use the stored access token
 
-            $client->setAccessToken($token['access_token']);
-            $service = new ShoppingContent($client);
+        $service = new ShoppingContent($client);
+        $result  = null;
 
-            foreach ($product_array as $add_product) {
-                $product = new ServiceProduct();
-                $product->setOfferId(substr($add_product['code'], 0, 50));
-                $product->setTitle($add_product['title']);
-                $product->setDescription($add_product['description']);
-                $product->setLink($add_product['link']);
-                $product->setImageLink($add_product['image_link']);
-                $product->setContentLanguage('en');
-                $product->setTargetCountry('US');
-                $product->setChannel('online');
-                $product->setAvailability($add_product['availability']);
-                $product->setCondition($add_product['condition']);
-                $product->setBrand($add_product['brand']);
-                $product->setGtin($add_product['barcode']);
-                $product->setMpn($add_product['code']);
-                $product->setAgeGroup('adult');
-                $product->setGender('unisex');
-
-                $shippingWeight = new \Google\Service\ShoppingContent\ProductShippingWeight();
-                $shippingWeight->setValue($add_product['product_weight']);
-                $shippingWeight->setUnit('lb');
-                $product->setShippingWeight($shippingWeight);
-
-                $price = new Price();
-                $price->setValue($add_product['price']);
-                $price->setCurrency('USD');
-                $product->setPrice($price);
-
-                $merchant_id = config('services.google.merchant_center_id');
-                $result = $service->products->insert($merchant_id, $product);
+        $productStatusList = [];
+        $feedIds = [];
+        $pageToken = null;
+        do {
+            try {
+                $products = $service->products->listProducts(config('services.google.merchant_center_id'), ['maxResults' => 250, 'pageToken' => $pageToken]);
+                foreach ($products->getResources() as $product) {
+                    $productId = $product['id'];
+                    $mpn = $product['mpn'];
+                    $productStatusList[] = [
+                        'id' => $productId,
+                        'mpn' => $mpn,
+                    ];
+                }
+                $pageToken = $products->getNextPageToken();
+            } catch (\Google\Service\Exception $e) {
+                report($e);
+                return $this->error('Failed to retrieve products from Google Merchant Center.');
             }
+        } while (!empty($pageToken));
+        if (!empty($product_array)) {
+            foreach ($product_array as $index => $add_product) {
+                $isDuplicate = false;
 
-            return $result;
+                foreach ($productStatusList as $existingProduct) {
+                    if ($add_product['code'] === $existingProduct['mpn']) {
+                        $isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!$isDuplicate) {
+                    $product = new ServiceProduct();
+                    $product->setOfferId(substr($add_product['code'], 0, 50));
+                    $product->setTitle($add_product['title']);
+                    $product->setDescription($add_product['description']);
+                    $product->setLink($add_product['link']);
+                    $product->setImageLink($add_product['image_link']);
+                    $product->setContentLanguage('en');
+                    $product->setTargetCountry('US');
+                    $product->setChannel('online');
+                    $product->setAvailability($add_product['availability']);
+                    $product->setCondition($add_product['condition']);
+                    $product->setBrand($add_product['brand']);
+                    // $product->setGoogleProductCategory($add_product['google_product_category']);
+                    $product->setGtin($add_product['barcode']);
+                    // $product->setmultipack('5000');
+                    // $product->setIdentifierExists(false);
+                    $product->setMpn($add_product['code']);
+                    $product->setAgeGroup('adult');
+                    // $product->setColor('universal');
+                    $product->setGender('unisex');
+                    // $product->setSizes(['Large']);
+
+                    $shippingWeight = new \Google\Service\ShoppingContent\ProductShippingWeight();
+                    $shippingWeight->setValue($add_product['product_weight']);
+                    $shippingWeight->setUnit('lb');
+                    $product->setShippingWeight($shippingWeight);
+
+                    $price = new Price();
+                    $price->setValue($add_product['price']);
+                    $price->setCurrency('USD');
+            
+                    $product->setPrice($price);
+                    $merchant_id = config('services.google.merchant_center_id');
+                    $result = $service->products->insert($merchant_id, $product);
+                } else {
+                    $product = new ServiceProduct();
+                    $product->setOfferId(substr($add_product['code'], 0, 50));
+                    $product->setTitle($add_product['title']);
+                    $product->setDescription($add_product['description']);
+                    $product->setLink($add_product['link']);
+                    $product->setImageLink($add_product['image_link']);
+                    $product->setContentLanguage('en');
+                    $product->setTargetCountry('US');
+                    $product->setChannel('online');
+                    $product->setAvailability($add_product['availability']);
+                    $product->setCondition($add_product['condition']);
+                    $product->setBrand($add_product['brand']);
+                    // $product->setGoogleProductCategory($add_product['google_product_category']);
+                    $product->setGtin($add_product['barcode']);
+                    // $product->setmultipack('5000');
+                    // $product->setIdentifierExists(false);
+                    $product->setMpn($add_product['code']);
+                    $product->setAgeGroup('adult');
+                    // $product->setColor('universal');
+                    $product->setGender('unisex');
+                    // $product->setSizes(['Large']);
+
+                    $shippingWeight = new \Google\Service\ShoppingContent\ProductShippingWeight();
+                    $shippingWeight->setValue($add_product['product_weight']);
+                    $shippingWeight->setUnit('lb');
+                    $product->setShippingWeight($shippingWeight);
+
+                    $price = new Price();
+                    $price->setValue($add_product['price']);
+                    $price->setCurrency('USD');
+            
+                    $product->setPrice($price);
+                    $merchant_id = config('services.google.merchant_center_id');
+                    $result = $service->products->insert($merchant_id, $product);
+                }
+
+            }
+            $this->info('Products inserted successfully.');
+            return $result;              
         } else {
+            $this->error('No products to insert.');
             return false;
         }
     }
