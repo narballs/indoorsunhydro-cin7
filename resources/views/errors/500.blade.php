0c1d5da555eb8d701b579259d2cf9d9357bf8dee
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-05-17 15:49:32 +0500

    fixing-order-checkout-issue

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Server Error</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+        .full-height {
+            height: 100vh;
+        }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+        .position-ref {
+            position: relative;
+        }
+        .code {
+            font-size: 26px;
+            padding: 0 15px;
+            text-align: center;
+        }
+        .message {
+            font-size: 18px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="flex-center position-ref full-height">
+        <div class="code">
+            500
+        </div>
+        <div class="message" style="padding: 10px;">
+            Oops! Something went wrong!<br>
+            <!-- Your custom text -->
+            We are working hard to fix this issue. Please try again later.
+        </div>
+    </div>
+</body>
+</html>
