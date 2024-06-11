517524398bb48ee1073d47e6c0b29c1d15fccd5a
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-06-12 01:47:18 +0500

    added changes for cin7 transactin

--- resources/views/partials/search-bar.blade.php
+++ resources/views/partials/search-bar.blade.php
@@ -105,16 +105,16 @@
 </div>
 
 
-<div class="w-100 mb-2 newsletter_div">
+<div class="w-100 mb-2 newsletter_div d-none ">
     @if (\Session::has('success'))
-        <div class="alert alert-success alert-dismissible fade show mt-2 text-center" role="alert">
+        <div class="alert alert-success alert-dismissible fade show mt-2 text-center d-none" role="alert">
             {!! \Session::get('success') !!}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
             </button>
         </div>
         @elseif (\Session::has('error'))
-        <div class="alert alert-danger alert-dismissible fade show mt-2 text-center" role="alert">
+        <div class="alert alert-danger alert-dismissible fade show mt-2 text-center d-none" role="alert">
             {!! \Session::get('error') !!}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
             <span aria-hidden="true">&times;</span>
