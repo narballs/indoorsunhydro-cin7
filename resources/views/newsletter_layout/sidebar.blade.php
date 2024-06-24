9725f81b55ad77b705b8d61a13a0d5492ef7200a
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-06-24 14:45:55 +0500

    added changes

--- resources/views/newsletter_layout/sidebar.blade.php
+++ resources/views/newsletter_layout/sidebar.blade.php
@@ -72,6 +72,15 @@
 					</ul>
 				</li>
 
+				<li class="nav-item  ">
+					<a href="{{url('admin/logout')}}" class="nav-link ">
+						<i class="nav-icon fas fa-sign-out-alt"></i>
+						<p>
+							Logout
+						</p>
+					</a>
+				</li> 
+
 			</ul>
 		</nav>
 		<!-- /.sidebar-menu -->
