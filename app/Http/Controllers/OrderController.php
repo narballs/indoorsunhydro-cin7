a01a9365c578996ae6a11c1ea0794e2909299e09
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-08-12 20:00:58 +0500

    added changes

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1320,6 +1320,8 @@ class OrderController extends Controller
         $order = ApiOrder::where('id', $order_id)->first();
         $previous_order_status = OrderStatus::where('id', $order->order_status_id)->first();
         $current_order_status = OrderStatus::where('id', $order_status_id)->first();
+        $cin7_auth_username = SettingHelper::getSetting('cin7_auth_username');
+        $cin7_auth_password = SettingHelper::getSetting('cin7_auth_password');
         if ($order->is_stripe === 1) {
             if (strtolower($current_order_status->status) === 'refunded') {
                 $stripe = new \Stripe\StripeClient(config('services.stripe.secret'));
@@ -1332,6 +1334,49 @@ class OrderController extends Controller
                             'order_status_id' => $order_status_id,
                             'isApproved' => $current_order_status->status == 'Refunded' ? 3 : $order->isApproved
                         ]);
+                        if (!empty($order->order_id)) {
+                            $client = new \GuzzleHttp\Client();
+                            $res = $client->request(
+                            'GET', 
+                                'https://api.cin7.com/api/v1/SalesOrders/' . $order->order_id,
+                                [
+                                    'auth' => [
+                                        $cin7_auth_username,
+                                        $cin7_auth_password
+                                    ]                    
+                                ]
+                            );
+
+                            $cin7_order = $res->getBody()->getContents();
+                            $get_order = json_decode($cin7_order);
+
+
+                            if (!empty($get_order)) {
+                                $is_approved_false = false;
+                                $is_voided = true;
+                                $curent_order_voided = $get_order->isVoid == false ? $get_order->isVoid : '';
+                                if ($curent_order_voided == false)  {
+                                    $res = $client->request(
+                                        'PUT', 
+                                        'https://api.cin7.com/api/v1/SalesOrders/' . $order->order_id,
+                                        [
+                                            'auth' => [
+                                                $cin7_auth_username,
+                                                $cin7_auth_password
+                                            ],
+                                            'json' => [
+                                                'isApproved' => false,
+                                                'isVoid' => true
+                                            ]
+                                        ]
+                                    );
+                                }
+                                
+                            }
+                        }
+                        
+
+
                     } else {
                         $request_status = false;
                         $message = 'Refund failed';
