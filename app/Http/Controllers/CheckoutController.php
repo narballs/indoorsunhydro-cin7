ba6583e3023fc3663f414f021bad71ef95974258
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-09-06 19:30:18 +0500

    added changes

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -1132,6 +1132,7 @@ class CheckoutController extends Controller
     }
 
     public function authenticate_user(Request $request) {
+
         $is_guest_user = !empty($request->is_guest) && $request->is_guest == 1 ? 1 : 0;  
 
         if ($is_guest_user == 1) {
@@ -1326,11 +1327,37 @@ class CheckoutController extends Controller
                 }
 
                 
-                
-
-
-                // I removed different_shipping from here
-                
+                $states = UsState::where('id', $request->state)->first();
+                $state_name = $states->state_name;
+                $toggle_registration = AdminSetting::where('option_name', 'toggle_registration_approval')->first();
+                $first_name = $request->first_name;
+                $last_name = $request->last_name;
+                $company = $request->company;
+                $different_shipping = $request->different_shipping_address;
+                $address1 = $request->address;
+                $address2 = $request->address_2;
+                $city = $request->city;
+                $postCode = $request->zip_code;
+                $phone = $request->phone;
+                if (!empty($request->postal_state)) {
+                    $postal_state = UsState::where('id', $request->postal_state)->first();
+                    $postal_state_name = $postal_state->state_name;
+                } else {
+                    $postal_state_name = '';
+                }
+                if ($different_shipping == 1) {
+                    $postalAddress1 = $request->postal_address1;
+                    $postalAddress2 = $request->postal_address2;
+                    $postalCity = $request->postal_city;
+                    $postalState = $postal_state_name;
+                    $postalPostCode = $request->postal_zip_code;
+                } else {
+                    $postalAddress1 = $address1;
+                    $postalAddress2 =$address2;
+                    $postalCity = $city;
+                    $postalState = $state_name;
+                    $postalPostCode =$postCode;
+                }
                 // $validatedAddress = UserHelper::validateAddress($address1, $state_name);
                 // dd($validatedAddress);
                 // if($validatedAddress != 'OK') {
@@ -1340,39 +1367,7 @@ class CheckoutController extends Controller
                 $useFirstCredentials = true;
                 while (true) {
                     DB::beginTransaction();
-                    
                     try {
-                        $states = UsState::where('id', $request->state)->first();
-                        $state_name = $states->state_name;
-                        $toggle_registration = AdminSetting::where('option_name', 'toggle_registration_approval')->first();
-                        $first_name = $request->first_name;
-                        $last_name = $request->last_name;
-                        $company = $request->company;
-                        $different_shipping = $request->different_shipping_address;
-                        $address1 = $request->address;
-                        $address2 = $request->address_2;
-                        $city = $request->city;
-                        $postCode = $request->zip_code;
-                        $phone = $request->phone;
-                        if (!empty($request->postal_state)) {
-                            $postal_state = UsState::where('id', $request->postal_state)->first();
-                            $postal_state_name = $postal_state->state_name;
-                        } else {
-                            $postal_state_name = '';
-                        }
-                        if ($different_shipping == 1) {
-                            $postalAddress1 = $request->postal_address1;
-                            $postalAddress2 = $request->postal_address2;
-                            $postalCity = $request->postal_city;
-                            $postalState = $postal_state_name;
-                            $postalPostCode = $request->postal_zip_code;
-                        } else {
-                            $postalAddress1 = $address1;
-                            $postalAddress2 =$address2;
-                            $postalCity = $city;
-                            $postalState = $state_name;
-                            $postalPostCode =$postCode;
-                        }
                         $api_password = $useFirstCredentials ? $cin7_auth_password1 : $cin7_auth_password2;
                         $price_column = null;
                         $default_price_column = AdminSetting::where('option_name', 'default_price_column')->first();
@@ -1412,16 +1407,16 @@ class CheckoutController extends Controller
                             'tax_class' => strtolower($state_name) == strtolower('California') ? '8.75%' : 'Out of State',
                             'paymentTerms' => 'Pay in Advanced',
                             'charge_shipping' => 1,
-                            'address1' => $address1,
-                            'address2' => $address2,
-                            'city' => $city,
-                            'state' => $state_name,
-                            'postCode' => $postCode,
-                            'postalAddress1' => $postalAddress1,
-                            'postalAddress2' => $postalAddress2,
-                            'postalCity' => $postalCity,
-                            'postalState' => $postalState,
-                            'postalPostCode' => $postalPostCode,
+                            'address1' => $postalAddress1,
+                            'address2' => $postalAddress2,
+                            'city' => $postalCity,
+                            'state' => $postalState,
+                            'postCode' => $postalPostCode,
+                            'postalAddress1' => $address1,
+                            'postalAddress2' => $address2,
+                            'postalCity' => $city,
+                            'postalState' => $state_name,
+                            'postalPostCode' => $postCode,
                             'accountsFirstName' => $user->first_name,
                             'accountsLastName' => $user->last_name,
                             'billingEmail' => SettingHelper::getSetting('noreply_email_address'),                                
@@ -1589,6 +1584,8 @@ class CheckoutController extends Controller
     
                         // Swap credentials
                         $useFirstCredentials = !$useFirstCredentials;
+
+                        // dd($different_shipping);
                     }
                 }
             }
