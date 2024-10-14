39dbac3acc6320b27415d044edec993a445a07ae
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-10-14 14:02:10 +0500

    admin-mobile-res

--- resources/views/emails/admin-order-received.blade.php
+++ resources/views/emails/admin-order-received.blade.php
@@ -316,7 +316,7 @@
                                     <thead>
                                         <tr>
                                             <td style="color:#000000;font-color:#000000;font-size: 14px; font-weight:600;">
-                                                Delievery Option
+                                                Delivery Option
                                             </td>
                                         </tr>
                                     </thead>
