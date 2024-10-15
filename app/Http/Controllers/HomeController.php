6b09bc5cd8a0f1a8c0e5bb15e166c02f6a97f061
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2024-10-15 18:05:10 +0500

    added review changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -260,7 +260,7 @@ class HomeController extends Controller
     private function calculateAverageRating($reviews)
     {
         if ($reviews->isEmpty()) {
-            return 'No reviews yet'; // Return a message if there are no reviews
+            return 'No Reviews'; // Return a message if there are no reviews
         }
         
         $totalRating = $reviews->sum('rating'); // Sum all the ratings
