5902e70c734223faa82963220b9765f27ad86c87
Engr Danish Sabir <engrdanishsabir00@gmail.com>
2025-04-15 17:39:50 +0500

    added changes

--- resources/views/admin/buy_list/list_row.blade.php
+++ resources/views/admin/buy_list/list_row.blade.php
@@ -1,4 +1,4 @@
-<tr id="product_row_{{ $product->product_id }}" class="product-row-{{ $product->product_id }} admin-buy-list">
+{{-- <tr id="product_row_{{ $product->product_id }}" class="product-row-{{ $product->product_id }} admin-buy-list">
 	<td>
 		{{ $product->name }}
 	</td>
@@ -22,4 +22,47 @@
         	<i class="fas fa-trash-alt cursor-pointer" onclick="deleteProduct({{$product->product_id }})"></i>
         </a>
 	</td>
+</tr> --}}
+
+
+
+@foreach($product->options as $option)
+@php
+	$retail_price = 0;
+	$user_price_column = App\Helpers\UserHelper::getUserPriceColumn();
+	foreach ($option->price as $price) {
+		$retail_price = $price->$user_price_column;
+		if ($retail_price == 0) {
+			$retail_price = $price->sacramentoUSD;
+		}
+		if ($retail_price == 0) {
+			$retail_price = $price->retailUSD;
+		}
+	}
+@endphp
+<tr id="product_row_{{ $product->product_id }}" class="product-row-{{ $product->product_id }} admin-buy-list">
+	<td>
+		{{ $product->name }}
+	</td>
+	<td>
+		{{-- @foreach($product->options as $option) --}}
+			<img src="{{$option->image}}" class="img-circle img-size-32 mr-2">
+			<input type="hidden" id="option_id_{{$product->product_id}}" value="{{ $option->option_id}}">
+		{{-- @endforeach --}}
+	</td>
+	<td>
+		$<span id="retail_price_{{ $product->product_id }}"> {{ $retail_price}} </span>
+	</td>
+	<td>
+		<input type="number" min="1" id="quantity_{{ $product->product_id }}" value="1" onclick="handleQuantity({{$product->product_id}})">
+	</td>
+	<td>
+		$<span id="subtotal_{{ $product->product_id }}"> {{ $retail_price * 1 }} </span>
+	</td>
+	<td>
+        <a class="cursor-pointer delete" title="" data-toggle="tooltip" data-original-title="Delete">
+        	<i class="fas fa-trash-alt cursor-pointer" onclick="deleteProduct({{$product->product_id }})"></i>
+        </a>
+	</td>
 </tr>
+@endforeach
