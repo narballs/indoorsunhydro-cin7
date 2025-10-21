9cc170123ef3c328fc8928ae329bfb763ae7e45a
Freddy Aliendres <freddy.aliendres@gmail.com>
2025-10-21 08:02:11 -0400

    fix adminlte

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+// Copy assets AdminLTE from vendor to public
+mix.copy('vendor/almasaeed2010/adminlte/dist', 'public/vendor/adminlte/dist');
